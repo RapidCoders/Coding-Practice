f5f16f38ea4a20861d819ab9dcddd215db38ebf6
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-02 11:29:46 +0530

    Create JavaScript_program.js

---
+++ DAY_1/Shreyash/Program2/JavaScript_program.js
@@ -0,0 +1,8 @@
+// Write a program to count number of digits in given number.
+// Input: 4365
+// Output: Number of digits are 4.
+
+// Input: 125
+// Output: Number of digits are 3.
+// Author: Suyash
+
