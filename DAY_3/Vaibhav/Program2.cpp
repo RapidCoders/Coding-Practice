58b9c09e578ee34cbd4ac2df7ccd615e32929e1a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-04 18:32:45 +0530

    Create Program2.cpp

---
+++ DAY_3/Vaibhav/Program2.cpp
@@ -0,0 +1,38 @@
+/*
+Q2. Write a program which accept one number from user and print that number of even numbers on screen.
+
+Input : 7
+Output: 2 4 6 8 10 12 14
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void print_n_even_nums(int n)
+{
+    int i, j = 2;
+
+    cout << "\nEven numbers are: ";
+    for (i = 1; i <= n; j++)
+    {
+        if (j % 2 == 0)
+        {
+            cout << j << " ";
+            i++;
+        }
+    }
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "\nEnter the number that you want to print even numbers: ";
+    cin >> num;
+
+    print_n_even_nums(num);
+
+    return 0;
+}
