fd165e01baab180cf92a238fc27973e8cb0bdbd1
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-04 21:24:25 +0530

    Update Program2.cpp

--- DAY_3/Shreyash/Program2.cpp
+++ DAY_3/Shreyash/Program2.cpp
@@ -1,4 +1,10 @@
 
+// Q2. Write a program which accept one number from user and print that number of even numbers on screen.
+// Input : 7
+// Output: 2 4 6 8 10 12 14
+// Author: Suyash
+
+
 
 
 // Code
