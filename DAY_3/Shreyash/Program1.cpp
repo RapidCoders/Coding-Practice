6739ef88168c6f4098817b9d7c863cc41ccd7d4d
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-12-04 17:07:50 +0530

    Create Program1.cpp

---
+++ DAY_3/Shreyash/Program1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int max_handshake(int num)
+{
+  int max_handshakes = num*(num-1)/2;
+  
+  return max_handshakes;
+}
+
+int main()
+{
+  int num;
+  cout<<"Enter Number of Peoples :"
+  c>>num;
+  return 0;
+}
