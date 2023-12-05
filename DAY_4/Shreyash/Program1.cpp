51d79ce3454c4f8df89d3d1a5a6ab779b5d5253b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-05 16:16:29 +0530

    Update Program1.cpp

--- DAY_4/Shreyash/Program1.cpp
+++ DAY_4/Shreyash/Program1.cpp
@@ -1,3 +1,13 @@
+// Q1. Write a program which accept rows and numbers of column from user and display below pattern.
+// Input : row = 3, col = 5
+// Output :
+// 5   4   3   2   1
+// 5   4   3   2   1
+// 5   4   3   2   1
+// Author: Vaibhav
+
+
+
 #include<iostream>
 using namespace std;
 
