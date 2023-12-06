5b4f73f7236bb6970b57f59647c438e59b58c793
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-06 09:00:32 +0530

    Update Program3.py

--- DAY_4/Shreyash/Program3.py
+++ DAY_4/Shreyash/Program3.py
@@ -9,22 +9,23 @@
 # Output: 21
 # Author: Shreyash
 
-def min_input():
-        arr = []
-        inputt = int(input("How many number you to input :"))
-        for i in range(1,inputt+1):
-            ele = int(input(f"Input {i} number :"))
-            arr.append(ele)
-
+def min_input(arr):
         min = arr[0]
         for j in range(len(arr)):
             if min>arr[j]:
                 min = arr[j]
-        print(f"Smallest number from your input is {min}")
+        
+        return min
 
 
 
 if __name__ == "__main__":
-     min_input()
-    
+     arr = []
+     inputt = int(input("How many number you to input :"))
+     for i in range(1,inputt+1):
+         ele = int(input(f"Input {i} number :"))
+         arr.append(ele)
+     print("Smallest number from your input is :",end=" ")
+     print(min_input(arr))
+
 
