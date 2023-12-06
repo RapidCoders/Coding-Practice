33bf938f5a8fefbe99940b7a9659b0b5653481c2
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-06 19:30:23 +0530

    Update Program1.cpp

--- DAY_5/Shreyash/Program1.cpp
+++ DAY_5/Shreyash/Program1.cpp
@@ -1,4 +1,18 @@
+// # Q1. Write a program which accept N numbers from user and display addition of digits of each element.
+// ~~~
+// Input: 5
+// Input: 85 36 98 125 225
+// Output: 13 9 17 8 9
 
+// Input: 7
+// Input: 854 698 236 14 75 32 65
+// Output: 17 23 14 5 12 5 11
+// ~~~
+// **Author: Vaibhav**
+
+ 
+
+// Code
 #include<iostream>
 #include <vector>
 #include <algorithm>
