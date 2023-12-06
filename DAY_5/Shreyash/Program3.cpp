72edf495dd4bde4247641f857d7e8da4ff282895
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-06 09:14:24 +0530

    Create Program3.cpp

---
+++ DAY_5/Shreyash/Program3.cpp
@@ -0,0 +1,36 @@
+
+
+
+#include<iostream>
+using namespace std;
+
+void relationShip(double num1,double num2)
+{
+    if(num1==num2){
+        cout<<"Numbers are same";
+    }
+    else if(num1>num2){
+        cout<<num1<<" is greater than "<<num2;
+    }
+    else if(num1<num2){
+        cout<<num2<<" is greater than "<<num2;
+    }
+    else{
+        cout<<"invalid input";
+    }
+    
+}
+
+int main()
+{
+
+    double num1;
+    double num2;
+    cout<<"Enter First Number :";
+    cin>>num1;
+    cout<<"Enter Second Number :";
+    cin>>num2;
+    relationShip(num1,num2);
+    
+    return 0;
+}
