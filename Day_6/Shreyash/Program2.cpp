2b2d6267be438873e8ce6a1ef5acd4e4575e8800
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-07 22:18:23 +0530

    Update Program2.cpp

--- Day_6/Shreyash/Program2.cpp
+++ Day_6/Shreyash/Program2.cpp
@@ -1,3 +1,10 @@
+// Q.2 Program to count the total number of punctuation characters exists in a string
+// Input: char str [] = "Good Morning! Mr. James Potter. Had your breakfast?"  
+// (If any character in the string is matched with ('!', "," ,"\'" ,";" ,"\"", ".", "-" ,"?"), increment the count by 1.)
+
+// Output: Total number of punctuation characters exists in string: 4
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
