b09e98ae18b49ab248a8adb838c6a54bc2653eba
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-12-07 22:20:02 +0530

    Create Program1.java

---
+++ Day_6/Suyash/Program1.java
@@ -0,0 +1,59 @@
+/*
+	Q.1 Write a program which accept rows and numbers of column from user and display below pattern.
+	Input : row = 6, col = 6
+	Output :
+	*   *   *   *   *   *   
+	*   *               *
+	*       *           *
+	*           *       *
+	*               *   *
+	*   *   *   *   *   *
+	Author: Vaibhav
+*/
+import java.util.Scanner;
+
+public class Program1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter number of rows");
+		int row = sobj.nextInt();
+		System.out.println("Enter number of columns");
+		int col = sobj.nextInt();
+		
+		PatternPrinting pobj = new PatternPrinting();
+		pobj.Pattern(row,col);
+		
+		sobj.close();
+	}
+}
+
+class PatternPrinting
+{
+	public void Pattern(int row, int col)
+	{
+		if(row != col)
+		{
+			System.out.println("Number of rows and columns shoud be same !");
+		}
+		else
+		{
+			for(int i=1; i<=row; i++)
+			{
+				for(int j=1; j<=col; j++)
+				{
+					if(i==1||j==1||i==row||j==col||i==j)
+					{
+						System.out.print("* ");
+					}
+					else
+					{
+						System.out.print("  ");
+					}	
+				}
+				System.out.println();
+			}
+		}
+	}
+}
