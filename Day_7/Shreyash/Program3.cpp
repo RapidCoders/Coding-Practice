6e4a682c2a22310b531f0fffd3ce1d464367c4ec
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-08 10:45:47 +0530

    Update Program3.cpp

--- Day_7/Shreyash/Program3.cpp
+++ Day_7/Shreyash/Program3.cpp
@@ -1,3 +1,15 @@
+
+// Q3. Write a Program for Printing Incrementing Number Square Pattern.
+// Input : row = 4, col = 6
+// Output:
+
+// 1 1 1 1 1 1 
+// 2 2 2 2 2 2
+// 3 3 3 3 3 3
+// 4 4 4 4 4 4
+
+// Author: Shreyash
+
 #include <iostream>
 using namespace std;
 void patt(int row,int col){
