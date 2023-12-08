2f38b761421e56a5b6c92658743f8e8d0d6a4107
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-08 08:55:50 +0530

    Update Program3.cpp

--- Day_7/Shreyash/Program3.cpp
+++ Day_7/Shreyash/Program3.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void patt(int row,int col){
+    if(row <= 0||col <=0){
+        cout<<"You have to enter Number greater than 0";
+    }
+    else{
+        for(int i =1;i<=row;i++){
+        for(int j = 1;j<=col;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    }
 
+    
+
+}
+ 
+int main(){
+    int row ;
+    int col ;
+    cout<<"Rows :";
+    cin>>row;
+    cout<<"Columns :";
+    cin>>col;
+    patt(row,col);
+
+}
