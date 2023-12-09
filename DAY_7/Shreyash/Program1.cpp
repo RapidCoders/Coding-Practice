5a407922721109f912f11f80b63d2857a37d4886
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-09 08:52:52 +0530

    Create Program1.cpp

---
+++ DAY_7/Shreyash/Program1.cpp
@@ -0,0 +1,38 @@
+// Q.1 Write a program which accept rows and numbers of column from user and display below pattern.
+// Input : row = 6, col = 6
+// Output :
+// *  *  *  *  *  *   
+// *           *  *
+// *        *     *
+// *     *        *
+// *  *           *
+// *  *  *  *  *  *
+// Author: Vaibhav
+
+#include<iostream>
+using namespace std;
+void patt(int rows,int column)
+{
+    for(int i = rows;i>=1;i--){
+        for(int j=1;j<=column;j++){
+            if(i==1||j==1||j==column||i==rows||i==j){
+                cout<<"*  ";
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+    }
+
+}
+
+int main()
+{
+    int rows;
+    int columns;
+    cin>>rows;
+    cin>>columns;
+    patt(rows,columns);
+
+}
