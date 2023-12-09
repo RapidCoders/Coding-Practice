21e49fa7c87a919e5d766dae04c190c072a09409
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-09 10:35:46 +0530

    Update Program2.cpp

--- DAY_8/Vaibhav/Program2.cpp
+++ DAY_8/Vaibhav/Program2.cpp
@@ -1,1 +1,45 @@
+/*
+Q.2 Program to print the elements of an array present on even position
 
+Input: arr = [1, 2, 3, 4, 5]  
+
+Output: Elements of given array present on even position: 1  3  5
+
+Author: Suyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void display_even_pos(int *arr, int n)
+{
+    int i;
+
+    cout << "Elements of given array present on even position: ";
+    for (i = 0; i < n; i+=2)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int i, n;
+    int *arr = NULL;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    arr = new int[n];
+
+    if (arr == NULL)
+        return -1;
+
+    cout << "Enter data: ";
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+
+    display_even_pos(arr, n);
+
+    return 0;
+}
