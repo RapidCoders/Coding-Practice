5e4ad6f395c1639cd608782f8188d52f65f448eb
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-09 11:04:32 +0530

    Update Program2.cpp

--- DAY_8/Shreyash/Program2.cpp
+++ DAY_8/Shreyash/Program2.cpp
@@ -1,3 +1,11 @@
+
+// Q.2 Program to print the elements of an array present on even position.
+// Input: arr = [1, 2, 3, 4, 5]  
+
+// Output: Elements of given array present on even position: 1 3 5 
+// Author: Suyash
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
