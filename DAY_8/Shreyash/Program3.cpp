6f1be98d07ae596ad6eef4f10b911a62b3145628
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-09 11:04:54 +0530

    Update Program3.cpp

--- DAY_8/Shreyash/Program3.cpp
+++ DAY_8/Shreyash/Program3.cpp
@@ -1,3 +1,31 @@
+
+// Q.3 Write a program which accept rows and numbers of column from user and display below pattern.
+// Input : row = 4, col = 4
+// Output :
+// *  *  *  *  
+// *  #  #  *  
+// *  #  #  *  
+// *  *  *  *  
+// Input : row = 6, col = 6
+// Output :
+// *  *  *  *  *  *  
+// *  #  #  #  #  *  
+// *  #  +  +  #  *  
+// *  #  +  +  #  *  
+// *  #  #  #  #  *  
+// *  *  *  *  *  * 
+// Input : row = 8, col = 8
+// Output :
+// *  *  *  *  *  *  *  *  
+// *  #  #  #  #  #  #  *  
+// *  #  +  +  +  +  #  *  
+// *  #  +        +  #  *  
+// *  #  +        +  #  *  
+// *  #  +  +  +  +  #  *  
+// *  #  #  #  #  #  #  *  
+// *  *  *  *  *  *  *  *  
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 void patt(int rows,int column)
