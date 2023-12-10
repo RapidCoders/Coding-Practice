03b3e37138f8bb9742a053f511170eeb6d94f2ab
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-11 00:49:22 +0530

    Update Program3.cpp

--- DAY_10/Shreyash/Program3.cpp
+++ DAY_10/Shreyash/Program3.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+void factors(int num)
+{
+    for(int i = 1;i<=num;i++)
+    {
+        if(num%i == 0){
+            cout<<i<<" ";
+        }
+        
+    }
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter Number :";
+    cin>>num;
+    factors(num);
+    return 0;
+
+}
