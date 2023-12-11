83b7f683dfc3959a292382484d3e9d145cb023da
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-11 10:31:54 +0530

    Update Program3.cpp

--- DAY_10/Shreyash/Program3.cpp
+++ DAY_10/Shreyash/Program3.cpp
@@ -1,23 +1,35 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void factors(int num)
+void factors(vector<int>arr)
 {
-    for(int i = 1;i<=num;i++)
-    {
-        if(num%i == 0){
-            cout<<i<<" ";
+    cout<<"factors :"<<endl;
+    for(int i :arr)
+    { 
+    
+        for(int k = 1;k<=i;k++){
+            if(i%k == 0)
+            {
+            cout<<k<<" ";
+            }
         }
-        
+        cout<<endl;
     }
 }
 
 int main()
 {
     int num;
+    vector<int>arr;
     cout<<"Enter Number :";
     cin>>num;
-    factors(num);
+    int temp;
+    for(int j =1;j<=num;j++){
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    factors(arr);
     return 0;
 
 }
