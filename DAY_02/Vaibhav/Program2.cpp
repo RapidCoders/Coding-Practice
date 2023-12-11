bac1d5b3ef14f70cc5bd47b65f86a7d4f00a6928
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-11 10:45:50 +0530

    Add files via upload

---
+++ DAY_02/Vaibhav/Program2.cpp
@@ -0,0 +1,42 @@
+/*
+Q2. Write a program to print given pattern
+
+Input: 5
+Output:
+*
+* *
+* * *
+* * * *
+* * * * *
+
+Author: Suyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void print_pattern(int n)
+{
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "Enter the number: ";
+    cin >> num;
+
+    print_pattern(num);
+
+    return 0;
+}
