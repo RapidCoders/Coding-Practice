2236d0cd4cc614e88adef967651da0eb54832950
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-11 10:43:13 +0530

    Update Program1.cpp

--- DAY_10/Shreyash/Program1.cpp
+++ DAY_10/Shreyash/Program1.cpp
@@ -1,30 +1,40 @@
 #include<iostream>
 using namespace std;
 
-void factors(int num)
+void patt(int row,int col)
 {
-    for(int i = 1;i<=num;i++)
+    if(row!=col)
+    {
+        cout<<"Invalid Output";
+    }
+    else{
+        for(int i = 1;i<=row;i++)
     {
         for(int j = 1;j<=i;j++){
             cout<<"* ";
         }
         cout<<endl;
     }
-    for(int k = num-1;k>=1;k--)
+    for(int k = row-1;k>=1;k--)
     {
         for(int x = 1;x<=k;x++){
             cout<<"* ";
         }
         cout<<endl;
     }
+    }
 }
 
 int main()
 {
-    int num;
-    cout<<"Enter Number :";
-    cin>>num;
-    factors(num);
+    int row;
+    cout<<"Enter rows :";
+    cin>>row;
+    int col;
+    cout<<"Enter rows :";
+    cin>>col;
+    cout<<endl;
+    patt(row,col);
     return 0;
 
 }
