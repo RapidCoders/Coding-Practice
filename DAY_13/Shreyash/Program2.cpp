17792ec1ef02ec3c45b46b05a27878caa90941af
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-14 01:26:11 +0530

    Update Program2.cpp

--- DAY_13/Shreyash/Program2.cpp
+++ DAY_13/Shreyash/Program2.cpp
@@ -1,3 +1,11 @@
+// Q.2 Program to perform user defined operation such as Addition, Subtraction, Multiplication, Division, suing Switch case.
+// Option 1 - Addtion 
+// Option 2 - Subtraction
+// Option 3 - Multiplication
+// Option 4 - Division
+// Take user defined input and Option and perform operation
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
