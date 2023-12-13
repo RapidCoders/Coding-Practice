11ada5cf4b8bedaf2c068d232506816bcd410c0b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-13 13:48:49 +0530

    Update Program1.cpp

--- DAY_12/Shreyash/Program1.cpp
+++ DAY_12/Shreyash/Program1.cpp
@@ -1,19 +1,19 @@
-Q.1 Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to the target.
-Input: nums = [2, 7, 11, 15], target = 9
-Output: [0, 1]
-Explanation: nums[0] + nums[1] equals 9, so the answer is [0, 1].
+// Q.1 Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to the target.
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1]
+// Explanation: nums[0] + nums[1] equals 9, so the answer is [0, 1].
 
-Input: nums = [3, 2, 4], target = 6
-Output: [1, 2]
-Explanation: nums[1] + nums[2] equals 6.
+// Input: nums = [3, 2, 4], target = 6
+// Output: [1, 2]
+// Explanation: nums[1] + nums[2] equals 6.
 
-Input: nums = [3, 2, 9, 7], target = 4
-Output: Not Found
+// Input: nums = [3, 2, 9, 7], target = 4
+// Output: Not Found
 
-Input: nums = [2,3,3,3,5], target = 6
-Output: [1, 2]
-Explanation: nums[1] + nums[2] equals 6.
-Author: Vaibhav
+// Input: nums = [2,3,3,3,5], target = 6
+// Output: [1, 2]
+// Explanation: nums[1] + nums[2] equals 6.
+// Author: Vaibhav
 
 #include<iostream>
 // #include<cmath>
