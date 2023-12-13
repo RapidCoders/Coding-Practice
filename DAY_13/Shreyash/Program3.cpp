28defd33bb1219af3f91fc30e379c6c9a66ac9bb
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-13 22:57:16 +0530

    Update Program3.cpp

--- DAY_13/Shreyash/Program3.cpp
+++ DAY_13/Shreyash/Program3.cpp
@@ -1,3 +1,16 @@
+// Q.3 Write a program which accept two numbers and check it is Friendly Pair or not.
+// Input :num1 = 30
+//        num2 = 140
+// Output : 30 & 140 are friendly pairs.
+
+// Input :num1 = 15
+//        num2 = 20
+// Output : 15 & 20 are not friendly pairs.
+
+// Friendly pair Explanation :(Sum of divisors of num1)/num1= (Sum of divisors of num2)/num2
+
+// Author: Shreyash
+
 #include <iostream>
 using namespace std;
 
