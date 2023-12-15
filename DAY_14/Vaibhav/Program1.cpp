b135104da2d81b5f587e3bbabbeb8706aad9d1af
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-15 10:26:55 +0530

    Update Program1.cpp

--- DAY_14/Vaibhav/Program1.cpp
+++ DAY_14/Vaibhav/Program1.cpp
@@ -1,1 +1,49 @@
+/*
+Q.1 Write a program which accept one number from user and check whether it is palindrome or not.
 
+Input : 121
+Output : Palindrome
+
+Input : 45654
+Output : Palindrome
+
+Input : 98765
+Output : Not Palindrome
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool check_palindrome(int num)
+{
+    int temp = num;
+    int new_num = 0;
+
+    while (temp != 0)
+    {
+        new_num = new_num * 10 + (temp % 10);
+        temp /= 10;
+    }
+    if (num == new_num)
+        return true;
+    return false;
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "\nEnter the number: ";
+    cin >> num;
+
+    if (check_palindrome(num))
+        cout << endl << num << " is palindrome number.\n";
+    else
+        cout << endl << num << " is not a palindrome number\n";
+
+    return 0;
+}
