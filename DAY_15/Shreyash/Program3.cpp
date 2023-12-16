8ed5bbeefdded7de884437993c83b1475d002ee2
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-16 08:39:49 +0530

    Create Program3.cpp

---
+++ DAY_15/Shreyash/Program3.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using namespace std;
+
+void patt(int row){
+    for(int i = 1;i<=row;i++){
+        for(int j = row;j>=i;j--){
+            if(i>=j||i==j||i+j==row+1){
+                cout<<"*";
+                
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+        
+    }
+}
+
+int main()
+{
+   
+    int row,col;
+    cin>>row;
+    cin>>col;
+    if(row == col){
+        patt(row);
+        
+    }else{
+        cout<<"Invalid Input";
+    }
+    return 0;
+}
