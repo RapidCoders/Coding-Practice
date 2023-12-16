2e8f3f3717657598732521cc8cb01e32c134176d
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-16 22:49:15 +0530

    Update Program2.cpp

--- DAY_15/Vaibhav/Program2.cpp
+++ DAY_15/Vaibhav/Program2.cpp
@@ -15,11 +15,6 @@ using std::cin;
         for (j = 1; j <= i; j++)
             cout << c++ << " ";
         cout << endl;
-    }
-}   
-
-int main(void)
-{
     int num;
 
     cout << "Enter the number: ";
