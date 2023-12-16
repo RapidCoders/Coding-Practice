4f9b0e4b0bb66eca34786ec858ac380459db2f7b
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-12-16 21:24:41 +0530

    Create Program3.java

---
+++ DAY_15/Suyash/Program3.java
@@ -0,0 +1,63 @@
+/*
+	Q.3 Write a program to print given pattern.
+	Input : row = 7 col = 7
+	Output :
+	
+	*           * 
+	  *       * 
+	    *   * 
+	      * 
+	    * 
+	  * 
+	* 
+	
+	Author: Shreyash
+*/
+import java.util.Scanner;
+
+public class Program3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows");
+		int row = sobj.nextInt();
+		System.out.println("Enter number of columns");
+		int col = sobj.nextInt();
+		
+		PrintingPattern2 pobj = new PrintingPattern2();
+		pobj.Pattern(row, col);
+		
+		sobj.close();
+	}
+}
+
+class PrintingPattern2
+{
+	public void Pattern(int row, int col)
+	{
+		if(row != col)
+		{
+			System.out.println("Number of rows and columns should be same");
+		}
+		else
+		{
+			for(int i=1; i<=row; i++)
+			{
+				for(int j=1; j<=col; j++)
+				{
+					if((i==j&&j<=col/2)||(i+j==row+1))
+					{
+						System.out.print("* ");
+					}
+					else
+					{
+						System.out.print("  ");
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
+}
