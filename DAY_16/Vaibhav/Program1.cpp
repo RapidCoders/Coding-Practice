a9512638249389cf2a20487ff18e6ad2ca9934e8
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-17 22:55:07 +0530

    Create Program1.cpp

---
+++ DAY_16/Vaibhav/Program1.cpp
@@ -0,0 +1,54 @@
+/*
+Q.1 Write a program to print below pattern.
+
+Input : row = 7 col = 7
+Output :
+
+* * * * * * *
+          *
+        *
+      *
+    *
+  *
+* * * * * * *
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void display_pattern(int row, int col)
+{
+    int i, j;
+
+    cout << endl;
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == row || j == col - i + 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}   
+
+int main(void)
+{
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    cout << "Enter the number of cols: ";
+    cin >> cols;
+
+    display_pattern(rows, cols);
+
+    return 0;
+}
