6efb87d3b35e5105eef628723904d0649c4c3353
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-17 07:43:13 +0530

    Create Program2.cpp

---
+++ DAY_15/Shreyash/Program2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void patt(int num){
+    char ch = 'A';
+    for (int i = 1; i <= num; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << ch << " ";
+            ++ch;
+        }
+        cout <<endl;
+    }
+}
+
+int main() {
+    int num;
+    cin>>num;
+    patt(num);
+    return 0;
+}
