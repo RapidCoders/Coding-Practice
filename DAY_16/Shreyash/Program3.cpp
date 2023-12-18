376fd5aafbadf664f688649e411e29d37dc577a2
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-18 09:48:45 +0530

    Create Program3.cpp

---
+++ DAY_16/Shreyash/Program3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void patt(int num) 
+{
+    for(int i = 1;i<=num;i++){
+      for(int j =1;j<=num;j++){
+        if(i==1||j==(num/2)+1){
+          cout<<"* ";
+        }else{
+          cout<<"  ";
+        }
+      }
+      cout<<endl;
+    }
+}
+
+int main(){
+  int num;
+  int num1;
+  cin>>num;
+  cin>>num1;
+  if(num==num1&&num%2 == 1&&num1%2==1){
+      patt(num);
+  }else{
+      cout<<"Invakid Input";
+  }
+  return 0;
+}
