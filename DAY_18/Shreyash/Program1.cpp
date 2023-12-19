287caaf1cf8ab06f8526d03c5cab1e9f9d9a2d4a
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-19 22:46:02 +0530

    Update Program1.cpp

--- DAY_18/Shreyash/Program1.cpp
+++ DAY_18/Shreyash/Program1.cpp
@@ -1,3 +1,13 @@
+// Q.1 Write a program to which accept radius(cm) of circle from the user and give circumference of circle.
+// Input : 5.5
+// Output : 34.54
+
+// Input: -3
+// Output: Invalid input.
+// Author: Vaibhav
+
+
+
 #include<iostream>
 using namespace std;
 
