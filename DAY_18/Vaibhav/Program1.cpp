f9d96c8c6e788cd79d90c7732cbcce024efba9b0
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-19 19:37:30 +0530

    Update Program1.cpp

--- DAY_18/Vaibhav/Program1.cpp
+++ DAY_18/Vaibhav/Program1.cpp
@@ -1,1 +1,31 @@
+/*
+Q.1 Write a program which accept radius from user and return the Circumference.
 
+Input : 5
+Output : 
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+#define PI 3.1415f
+using std::cin;
+using std::cout;
+using std::endl;
+
+float Circumference(float radius)
+{
+    return 2 * PI * radius;
+}
+
+int main(void)
+{
+    float radius;
+
+    cout << "\nEnter the radius: ";
+    cin >> radius;
+
+    cout << "\nCircumference of circle is: " << Circumference(radius) << endl;
+
+    return 0;
+}
