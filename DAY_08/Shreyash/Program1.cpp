41753d9bd44f06299733b1c54bd7f92476991864
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:13:10 +0530

    Update Program1.cpp

--- DAY_08/Shreyash/Program1.cpp
+++ DAY_08/Shreyash/Program1.cpp
@@ -15,11 +15,15 @@
 using namespace std;
 void patt(int rows)
 {
-    for(int i =1;i<=rows;i++){
-        for(int j=rows;j>=1;j--){
+    for(int i =1;i<=rows;i++)
+    {
+        for(int j=rows;j>=1;j--)
+        {
             
-            if(j == i){
-                for(int k = 1;k<=j;k++){
+            if(j == i)
+            {
+                for(int k = 1;k<=j;k++)
+                {
                     cout<<"*";
                 }
             }
