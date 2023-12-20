6b230bb7e1dff2a6f24bed8a3bf1b3486527b4db
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:28:26 +0530

    Update Program1.cpp

--- DAY_13/Shreyash/Program1.cpp
+++ DAY_13/Shreyash/Program1.cpp
@@ -15,9 +15,12 @@
 #include <iostream>
 using namespace std;
 
-int indexfinder(string str,char charr){
-	for(int i = 0;i<=str.length();i++){
-		if(str[i] == charr){
+int indexfinder(string str,char charr)
+{
+	for(int i = 0;i<=str.length();i++)
+	{
+		if(str[i] == charr)
+		{
 			return i;
 		}
 	}
@@ -31,9 +34,12 @@ int main()
 	getline(cin,str); 
 	cin>>charr;
 	int idx = indexfinder(str,charr);
-	if (idx != -1) {
+	if (idx != -1) 
+	{
         cout<<idx<<endl;
-        } else {
+        } 
+	else 
+	{
         cout <<-1<<endl;
         }
 	return 0;
