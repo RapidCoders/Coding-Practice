bee6305f0d556a276ac2e4c7c11654b9fef2c2f1
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:07:46 +0530

    Update Program2.cpp

--- DAY_19/Shreyash/Program2.cpp
+++ DAY_19/Shreyash/Program2.cpp
@@ -1,3 +1,11 @@
+// Q.2 Program to print the average of n numbers.
+// For example: 1,2,3,4,5
+// Number of all elements = 5
+// Sum of all elements = 1+2+3+4+5 =15
+// Average = Sum of all elements / number of all elements = 15/5 =3
+// Average =3
+// Author: Suyash
+
 #include <iostream>
 #include<vector>
 using namespace std;
