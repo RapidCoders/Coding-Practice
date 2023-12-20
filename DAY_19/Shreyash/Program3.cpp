18bf0819d59edb68d9c02f4fae94f9ced68c198b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:08:02 +0530

    Update Program3.cpp

--- DAY_19/Shreyash/Program3.cpp
+++ DAY_19/Shreyash/Program3.cpp
@@ -1,3 +1,17 @@
+// Q.3 Write a Program to check if number is Harshad number or not.
+// Input : num = 21
+// Output : True
+
+// it is divisible by its own sum (1+2) of its digit(2,1)
+// So it is Harshad's Number
+
+// Input : num = 31
+// Output : False
+
+// it is not divisible by its own sum (3+1) of its digit(3,1)
+// So it is not Harshad's Number
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
