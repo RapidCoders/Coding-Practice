d5cb380ff7cf97148d60ea905f3f46054ce7713f
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:11:31 +0530

    Update Program1.cpp

--- DAY_05/Shreyash/Program1.cpp
+++ DAY_05/Shreyash/Program1.cpp
@@ -22,10 +22,12 @@ using namespace std;
 void sum(vector<int>arr)
 {
     vector<int>arr2;
-    for(int j:arr){
+    for(int j:arr)
+    {
         int sum = 0;
         int r ;
-        while(j>0){
+        while(j>0)
+        {
             r = j%10;
             sum = sum + r;
             j = j/10;
@@ -34,7 +36,8 @@ void sum(vector<int>arr)
         arr2.push_back(sum);
 
     }
-    for(int k:arr2){
+    for(int k:arr2)
+    {
         cout<<k<<" ";
     }
 
@@ -47,7 +50,8 @@ int main()
     cout<<"How Many Number's frequency do you want to check :";
     cin>>num;
     int temp;
-    for(int i=1;i<=num;i++){
+    for(int i=1;i<=num;i++)
+    {
          cin>>temp;
          arr.push_back(temp);
 
