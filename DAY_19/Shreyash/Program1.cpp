71576a27991ff04966dfa117bdcc62442a15cb15
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:07:13 +0530

    Create Program1.cpp

---
+++ DAY_19/Shreyash/Program1.cpp
@@ -0,0 +1,21 @@
+// Q.1 Write a program to check if a given number is a perfect square.
+// A perfect square is a non-negative integer that can be expressed as the product of an integer multiplied by itself. In other words, a positive integer (n) is a perfect square if there exists an integer (m) such that (n = m \times m).
+
+// For example:
+
+// 1 is a perfect square because (1 = 1 times 1).
+// 4 is a perfect square because (4 = 2 times 2).
+// 9 is a perfect square because (9 = 3 times 3).
+// 16 is a perfect square because (16 = 4 times 4).
+// Input: 9
+// Output: True
+
+// Input: 25
+// Output: True
+
+// Input: 7 
+// Output: False
+
+// Input: 18
+// Output: False
+// Author: Vaibhav
