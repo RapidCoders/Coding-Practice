adf2172c79fd16cd07dd18c15aa75f4960230ddc
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-20 12:16:06 +0530

    Update Program1.cpp

--- DAY_09/Shreyash/Program1.cpp
+++ DAY_09/Shreyash/Program1.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 void patt(int rows,int cols)
 {
-    if(rows != cols){
+    if(rows != cols)
+    {
         cout<<"invalid input! Enter numbers of rows and columns same.";
     }
     else{
-        for(int i =1;i<=rows;i++){
+        for(int i =1;i<=rows;i++)
+        {
         
-        for(int j=rows;j>=1;j--){
+        for(int j=rows;j>=1;j--)
+        {
             
-            if(j == i){
-                for(int k = 1;k<=j;k++){
+            if(j == i)
+            {
+                for(int k = 1;k<=j;k++)
+                {
                     cout<<"* ";
                 }
             }
-            else{
+            else
+            {
                 cout<<" ";
             }
            
