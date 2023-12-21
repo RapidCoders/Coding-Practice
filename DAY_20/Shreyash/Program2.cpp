e7c516f9ef86b08e135e85e7749638ba57319949
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-21 16:34:04 +0530

    Update Program2.cpp

--- DAY_20/Shreyash/Program2.cpp
+++ DAY_20/Shreyash/Program2.cpp
@@ -1,3 +1,17 @@
+// Q.2 Program to check whether number is pronic or not.
+// A number is said to be pronic number if it is a product of two consecutive numbers.
+
+// For examples:
+// 6 = 2 x 3
+// 72 = 8 x 9
+
+// Input : 6
+// Output : It is a pronic number
+
+// Input : 7
+// Output : It is not a pronic number
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
