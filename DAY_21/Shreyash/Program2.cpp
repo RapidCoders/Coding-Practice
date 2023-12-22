1a96488bff3816fbc9944c55a116399fd6bb3f0b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-22 23:32:44 +0530

    Update Program2.cpp

--- DAY_21/Shreyash/Program2.cpp
+++ DAY_21/Shreyash/Program2.cpp
@@ -1,3 +1,16 @@
+
+// Q.2 Program to check whether number is Krishnamurthy number or not.
+// Krishnamurthy number is another special number in Java. A number is said to be Krishnamurthy if the factorial sum of all its digits is equal to that number. Krishnamurthy number is also referred to as a Strong number.
+
+// Example : 
+// Number = 145  
+// = 1! + 4! + 5!  
+// = 1 + ( 4 * 3 * 2 * 1 ) + ( 5 * 4 * 3 * 2 * 1 )  
+// = 1 + 24 + 120  
+// = 145
+ 
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
