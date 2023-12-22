47ed3a92bbb6adf445929e27a0e6795b0accd0a9
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-22 23:30:15 +0530

    Create Program3.cpp

---
+++ DAY_21/Shreyash/Program3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void patt(int rows)
+{
+    if(rows%2 == 0)
+    {
+        cout<<"invalid input(only odd value accepted)";
+    }
+    else{
+        for(int i = 1;i<=rows;i = i+2){
+			for(int j = 1;j<=(rows - i)/2;j++){
+				cout<<"  ";
+			}
+			for(int k = 1;k<=i;k++){
+				cout<<"* ";
+			}
+			cout<<endl;
+		}
+		for(int i = rows-2;i>=1;i = i-2){
+			for(int j = 1;j<=(rows - i)/2;j++){
+				cout<<"  ";
+			}
+			for(int k = 1;k<=i;k++){
+				cout<<"* ";
+			}
+			cout<<endl;
+		}
+
+    }
+}
+
+int main()
+{
+    int rows;
+    cout<<"Enter Rows :";
+    cin>>rows;
+    patt(rows);
+
+}
