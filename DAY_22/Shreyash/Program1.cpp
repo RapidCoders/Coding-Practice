809a9d70ef43373c977bb3c43154a381293b6951
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-23 09:30:23 +0530

    Create Program1.cpp

---
+++ DAY_22/Shreyash/Program1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int apha_counter(string str)
+{
+    int count = 0;
+    for(char i : str){
+        if(i == ' '){
+            count++;
+        }
+    }
+    return count+1;
+}
+
+int main(){
+
+    string str;
+    cout<<"Enter Input String :";
+    getline(cin,str);
+    int count = apha_counter(str);
+    // cout<<count<<endl;
+    if(count > 1){
+        cout<<"Count :"<<count;
+    }else{
+        cout<<0;
+    }
+    return 0;
+    
+}
