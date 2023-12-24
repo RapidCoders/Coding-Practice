169715234761aa438b72754dd7d9c3ae5b89c627
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-24 22:49:20 +0530

    Update Program1.cpp

--- DAY_23/Shreyash/Program1.cpp
+++ DAY_23/Shreyash/Program1.cpp
@@ -1,3 +1,19 @@
+// Q.1 Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+// The function should return a string, the longest common prefix of all strings in the input list.
+
+// Input: 4
+// Input: ["apple", "apricot", "apology", "app"]
+// Output: "ap"
+
+// Input: 4
+// Input: ["python", "java", "javascript", "c++"]
+// Output: ""
+
+// Input: 3
+// Input: ["testing", "test", "tester"]
+// Output: "test"
+// Author: Vaibhav
+
 #include <iostream>
 #include <vector>
 using namespace std;
