4f5ff3263167d85d370d83374ee74102983560db
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-24 11:35:25 +0530

    Update Program3.cpp

--- DAY_23/Shreyash/Program3.cpp
+++ DAY_23/Shreyash/Program3.cpp
@@ -1,3 +1,16 @@
+// Q.3 Write a program to print given pattern.
+// Input : 7
+// Output :
+//            1
+//            2*2
+//            3*3*3
+//            4*4*4*4
+//            3*3*3
+//            2*2
+//            1
+
+// Author: Shreyash
+
 #include <iostream>
 using namespace std;
 
