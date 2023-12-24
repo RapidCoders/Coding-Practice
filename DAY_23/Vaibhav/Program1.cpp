b77d16f619d6df39061925c63ec312f9240c40e1
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-24 21:16:23 +0530

    Add files via upload

--- DAY_23/Vaibhav/Program1.cpp
+++ DAY_23/Vaibhav/Program1.cpp
@@ -1,1 +1,59 @@
+/*
+Q.1 Write a program which accept a string from user and return the count of number of words in that string.
 
+Input: Maze gaon kokan ahe. Mazya gavashejari samudra ahe.
+Output: 8
+
+Input:
+Output: 0
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+string largest_common_prefix(vector<string> &strings)
+{
+    int i, j;
+    unordered_map<char, int> map;
+
+    for (i = 0; i < strings.size(); i++)
+    {
+        for (j = 0; j < strings[i].size(); j++)
+        {
+            map['A']++;
+        }
+    }
+}
+
+int main(void)
+{
+    int i, n;
+    string str;
+    vector<string> strings;
+
+    cout << "Enter the number of strings: ";
+    cin >> n;
+
+    cout << "Enter the strings one by one: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> str;
+        strings.push_back(str);
+    }
+
+    str = largest_common_prefix(strings);
+
+    if (!str.empty())
+        cout << "Common largest prefix of strings is: " << str << endl;
+
+    return 0;
+}
