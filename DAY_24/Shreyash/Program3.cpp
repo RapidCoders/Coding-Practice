cf8455b62fedfe2a9a31a7d91a5a74b1ab72d048
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-26 01:02:35 +0530

    Update Program3.cpp

--- DAY_24/Shreyash/Program3.cpp
+++ DAY_24/Shreyash/Program3.cpp
@@ -1,3 +1,17 @@
+// Q.3 Find minimum and maximum element in an array.
+// Input:
+// N = 6
+// A[] = {3, 2, 1, 56, 10000, 167}
+// Output: 1 10000
+// Explanation: minimum and maximum elements of array are 1 and 10000.
+
+// Input:
+// N = 5
+// A[]  = {1, 345, 234, 21, 56789}
+// Output: 1 56789
+// Explanation: minimum and maximum element of array are 1 and 56789.
+// Author: Shreyash
+
 #include <iostream>
 #include <vector>
 using namespace std;
