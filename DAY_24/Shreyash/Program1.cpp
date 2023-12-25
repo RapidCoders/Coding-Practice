99a1151d7e7c5e063440a60b41de4b137c079f6e
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-26 00:58:43 +0530

    Update Program1.cpp

--- DAY_24/Shreyash/Program1.cpp
+++ DAY_24/Shreyash/Program1.cpp
@@ -1,3 +1,27 @@
+// Q.1 You are given an array containing n distinct numbers taken from the range 0 to n. One number is missing from the array. Write a function to find and return the missing number.
+// Constraints: 1 <= arr[i] <= 100
+
+// Input: 5
+// Input: [1, 2, 3, 4, 6]
+// Output: 5
+
+// Input: 8
+// Input: [1, 2, 3, 4, 5, 6, 7, 9]
+// Output: 8
+
+// Input: 4
+// Input: [5, 2, 4, 1]
+// Output: 3
+
+// Input: 8
+// Input: [8, 3, 6, 1, 5, 2, 4, 9]
+// Output: 7
+
+// Input: 8
+// Input: [2, 1, 4, 5, 3, 8, 7, 6]
+// Output: 9
+// Author: Vaibhav
+
 #include <iostream>
 #include <vector>
 using namespace std;
