aba178c3c8d0d0dace19f516db0d3d01148c8d8b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-26 00:57:43 +0530

    Update Program2.cpp

--- DAY_24/Shreyash/Program2.cpp
+++ DAY_24/Shreyash/Program2.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int median_even(vector<int>arr,int num)
+{
+    int sum = arr[arr.size()/2] + arr[(arr.size()/2)+1];
+    return sum/2;
+}
+int median_odd(vector<int>arr,int num)
+{
+    int sum = arr[arr.size()/2];
+    return sum;
+    
+}
+
+
+int main() {
+    vector<int>arr;
+    int num;
+    cout << "Number: ";
+    cin >> num;
+    int temp;
+    for(int i = 1;i<=num;i++){
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int even = median_even(arr,num);
+    int odd = median_odd(arr,num);
+    if(num%2 == 0){
+        cout<<even;
+    }else{
+        cout<<odd;
+    }
+    return 0;
+}
 
