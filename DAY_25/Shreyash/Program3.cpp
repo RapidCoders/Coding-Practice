9c0769956b84e7dee0ca688115afa7478a42f6fd
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-26 21:46:35 +0530

    Update Program3.cpp

--- DAY_25/Shreyash/Program3.cpp
+++ DAY_25/Shreyash/Program3.cpp
@@ -1,3 +1,13 @@
+// Q.3 Write a Program to find Length of Last Word of user defind string.
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 using namespace std;
