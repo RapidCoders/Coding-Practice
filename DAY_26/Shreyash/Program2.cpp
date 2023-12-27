b2a9a54155c966518e9786805c87eb5cb5b1a627
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-27 10:36:50 +0530

    Update Program2.cpp

--- DAY_26/Shreyash/Program2.cpp
+++ DAY_26/Shreyash/Program2.cpp
@@ -1,13 +1,13 @@
-Q.2 Program to determine whether a given matrix is an identity matrix.
-Explanation : A matrix is said to be the identity matrix if it is the square matrix in which elements of principle diagonal are ones,
-              and the rest of the elements are zeroes.
-Input:
-Matrix a =[1, 0, 0]  
-          [0, 1, 0]  
-          [0, 0, 1]  
+// Q.2 Program to determine whether a given matrix is an identity matrix.
+// Explanation : A matrix is said to be the identity matrix if it is the square matrix in which elements of principle diagonal are ones,
+//               and the rest of the elements are zeroes.
+// Input:
+// Matrix a =[1, 0, 0]  
+//           [0, 1, 0]  
+//           [0, 0, 1]  
 
-Output: Given matrix is an identity matrix
-Author: Suyash
+// Output: Given matrix is an identity matrix
+// Author: Suyash
 
 #include<iostream>
 using namespace std;
@@ -15,10 +15,14 @@ using namespace std;
 void summ(int mat1[3][3], const int row, const int col)
 {
     int counter = 0;
-    for(int i = 0;i<row;i++){
-        for(int j = 0;j<col;j++){
-            if( i == j){
-                if(mat1[i][j] == 1){
+    for(int i = 0;i<row;i++)
+    {
+        for(int j = 0;j<col;j++)
+        {
+            if( i == j)
+            {
+                if(mat1[i][j] == 1)
+                {
                     counter++;
                 }
             }
@@ -43,8 +47,10 @@ int main()
     int const col = 3;
     int mat1[row][col];
     cout<<"Enter Data of Matrix One :";
-    for(int i = 0;i<row;i++){
-        for(int j = 0;j<col;j++){
+    for(int i = 0;i<row;i++)
+    {
+        for(int j = 0;j<col;j++)
+        {
             cin>>mat1[i][j];
         }
     }
