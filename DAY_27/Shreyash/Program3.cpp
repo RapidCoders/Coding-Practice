ab8478ae7b4fc788687ca8f218fb277b0c31132b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-28 16:20:38 +0530

    Update Program3.cpp

--- DAY_27/Shreyash/Program3.cpp
+++ DAY_27/Shreyash/Program3.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 void remover(string num){
     string new_eq = "";
-    for(int i = 0;i<=num.size();i++){
-        if(num[i] == '('||num[i] == ')'||num[i] == '{'||num[i] =='}'||num[i]=='['||num[i] == ']'){
+    for(int i = 0;i<=num.size();i++)
+    {
+        if(num[i] == '('||num[i] == ')'||num[i] == '{'||num[i] =='}'||num[i]=='['||num[i] == ']')
+        {
             continue;
-        }else{
+        }else
+        {
             new_eq = new_eq + num[i];
         }
     }
