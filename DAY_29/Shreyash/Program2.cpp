d292b468d79a3058448b8a8b25bc9e556f09bc61
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-30 16:06:11 +0530

    Update Program2.cpp

--- DAY_29/Shreyash/Program2.cpp
+++ DAY_29/Shreyash/Program2.cpp
@@ -1,3 +1,16 @@
+// Q.2 Write a program to check whether given number is Perfect number or not.
+// Explanation: 
+// Let's take the number 496 and heck it is a perfect number or not.
+
+// First, we find the factors of 496 i.e. 1, 2, 4, 8, 16, 31, 62, 124, and 248. Let's find the sum of factors (1 + 2 + 4 + 8 + 16 + 31 + 62 + 124 +248 = 496). We observe that the sum of factors is equal to the number itself. Hence, the number 496 is a perfect number.
+
+// Input : Enter the number : 28
+// Output : 28 is a perfect number.
+
+// Input : Enter the number : 4558
+// Output : 4558 is not a perfect number.
+// Author: Suyash
+
 #include <iostream>
 #include <vector>
 using namespace std;
