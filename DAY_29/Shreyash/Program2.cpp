4c4ff96501c5bf99d23e33e2195e7f72d8f1ff8d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-30 09:04:36 +0530

    Create Program2.cpp

---
+++ DAY_29/Shreyash/Program2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool perfect_num(int num){
+	int sum = 0;
+	for(int i = 1;i<num;i++){
+		if(num%i == 0){
+			sum = sum + i;
+			cout<<i<<" ";
+		}
+	}
+	if(sum == num){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
+int main() {
+    vector<int>arr;
+    int num;
+    cout << "Number: ";
+    cin >> num;
+    bool flag = perfect_num(num);
+	if(flag){
+		cout<<"True";
+	}else{
+		cout<<"False";
+	}
+
+}
