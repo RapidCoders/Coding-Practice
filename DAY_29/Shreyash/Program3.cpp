2b88fab126dc96d12107251a3c722d7285a0dcd5
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-30 16:06:26 +0530

    Update Program3.cpp

--- DAY_29/Shreyash/Program3.cpp
+++ DAY_29/Shreyash/Program3.cpp
@@ -1,3 +1,18 @@
+Q.3 Write a program to check number is Abundant Number or Not.
+Input: 12
+Output: True.
+Explanation:
+12 having a proper divisor is 1, 2, 3, 4, 6 
+
+The sum of these factors is 16 it is greater than 12 
+So it is an Abundant number
+
+Some other abundant numbers: 
+
+18, 20, 30, 70, 78, 80, 84, 90, 96, 100, 104, 108, 120
+
+Author: Shreyash
+
 #include <iostream>
 #include <vector>
 using namespace std;
