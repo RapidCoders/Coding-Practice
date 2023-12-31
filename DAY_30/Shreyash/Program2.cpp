d1d19d2daab289f39e01b6e1013d7887608313a3
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-31 12:01:44 +0530

    Update Program2.cpp

--- DAY_30/Shreyash/Program2.cpp
+++ DAY_30/Shreyash/Program2.cpp
@@ -1,3 +1,20 @@
+
+// Q.2 Write a program to check whether given number is Fascinating number or not.
+// Example :
+// Let's take any number (n) say 327 and check whether the given number is fascinating or not. On multiplying the given number (n) by 2 and 3, we get:
+
+// 327×2=654
+
+// 327×3=981
+
+// Now, concatenate the above results to the given number (n).
+
+// "327"+"654"+ "981"= 327654981
+
+// We observe that the resultant () contains all the digits from 1 to 9, exactly once. Hence, the given number 327 is a fascinating number. Note that, we have not added the result to the given number. Some other fascinating numbers are 192, 219, 273, 327, 1902, 1920, 2019 etc.
+// Author: Suyash
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
