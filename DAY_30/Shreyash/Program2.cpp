e2efe3ed3ce86aca8e91bc42efccacb9b74c2a96
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-31 11:08:37 +0530

    Update Program2.cpp

--- DAY_30/Shreyash/Program2.cpp
+++ DAY_30/Shreyash/Program2.cpp
@@ -8,12 +8,18 @@ bool fascinating(int num){
 	string num_str = to_string(num)+to_string(two)+to_string(three);
 	// cout<<num_str;
 	int count = 0;
-	for(char i : num_str){
-		if(i == '1'||i == '2'||i == '3'||i == '4'||i == '5'||i == '6'||i == '7'||i == '8'||i == '9'){
-			count++;
+
+	for(int j = '1';j<='9';j++){
+		for(char i : num_str)
+		{
+			if(j == i){
+				count++;
+				break;
+			}
+		
 		}
 	}
-	// cout<<" "<<count<<" ";
+	cout<<" "<<count<<" ";
 	if(count == 9){
 		return true;
 	}else{
