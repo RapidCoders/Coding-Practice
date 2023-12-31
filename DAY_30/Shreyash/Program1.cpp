1955144da6d985569a7c5c86f326eb242b28ee4f
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-31 12:01:24 +0530

    Update Program1.cpp

--- DAY_30/Shreyash/Program1.cpp
+++ DAY_30/Shreyash/Program1.cpp
@@ -1,3 +1,15 @@
+// Q.1 Write a program that takes a matrix (2D array) as input and returns its transpose.
+// Input: row = 3, col = 3
+// Input:
+//     1 2 3
+//     4 5 6
+//     7 8 9
+
+// Output:
+//     1 4 7
+//     2 5 8
+//     3 6 9
+// Author: Vaibhav
 
 
 #include<iostream>
