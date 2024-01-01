57191fc0d3cfa8f39e27eacca788ba32defd9ca8
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-01 19:24:24 +0530

    Update Program3.cpp

--- DAY_31/Shreyash/Program3.cpp
+++ DAY_31/Shreyash/Program3.cpp
@@ -1,3 +1,12 @@
+
+// Q.3 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Author: Shreyash
+
 #include <iostream>
 #include <vector>
 using namespace std;
