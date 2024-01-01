6eeba9b4b91fd136103b295cdf4261030e52bddf
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-01 19:22:55 +0530

    Create Program1.cpp

---
+++ DAY_31/Shreyash/Program1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void patt(int num) 
+{
+    for(int i = 1;i<=num/2;i++){
+      for(int j =1;j<=num/2+1;j++){
+        if(j==1||j==num/2+1){
+          cout<<" *";
+        }else{
+          cout<<"  ";
+        }
+      }
+	  for(int j =1;j<=num/2;j++){
+        if(i==1){
+          cout<<" *";
+        }else{
+          cout<<"  ";
+        }
+      }
+	  cout<<endl;
+    }
+	for(int i = 1;i<=num;i++){
+		cout<<" *";
+	}
+	cout<<endl;
+	for(int i = 1;i<=num/2;i++){
+		for(int j =1;j<=num/2;j++){
+        if(i==num/2){
+          cout<<" *";
+        }else{
+          cout<<"  ";
+        }
+      }
+	  for(int j =1;j<=num/2+1;j++){
+        if(j==num/2+1||j==1){
+          cout<<" *";
+        }else{
+          cout<<"  ";
+        }
+      }
+	  cout<<endl;
+	}
+	  
+}
+
+int main(){
+  int num;
+  cin>>num;
+  patt(num);
+  return 0;
+}
