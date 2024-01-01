b80176fd67384e4d407eef34992da52a5d5b9a44
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-01 18:54:17 +0530

    Create Program3.cpp

---
+++ DAY_31/Shreyash/Program3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void once_occurence(vector<int>arr){
+	for(int i=0; i<arr.size(); i++)
+	{
+		int counter = 0;
+		for(int j=0; j<arr.size(); j++)
+		{
+			if(arr[i] == arr[j])
+			{
+				counter++;
+			}
+		}
+		if(counter == 1)
+			cout<<arr[i];
+	}
+	
+}
+
+int main() {
+    vector<int>arr;
+    int num;
+    cout << "Number: ";
+    cin >> num;
+    int temp;
+	for(int i = 1;i<=num;i++){
+		cin>>temp;
+		arr.push_back(temp);
+	}
+
+	once_occurence(arr);
+
+
+}
