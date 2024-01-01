e9b5fc6485be0d1c993ee43e55df239cc62e218a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-01 23:40:06 +0530

    Add files via upload

--- DAY_31/Vaibhav/Program1.cpp
+++ DAY_31/Vaibhav/Program1.cpp
@@ -1,1 +1,53 @@
+/*
+Q.1 Write a program that prints swastika pattern.
 
+Input: 7
+Output:
+
+*     * * * *
+*     *
+*     *
+* * * * * * *
+      *     *
+      *     *
+* * * *     *
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void print_pattern(int num)
+{
+    int i, j;
+
+    if (num % 2 == 0)
+        return;
+
+    for (i = 1; i <= num; i++)
+    {
+        for (j = 1; j <= num; j++)
+        {
+            if (i == (num / 2) + 1 || j == (num / 2) + 1 || (i == 1 && j > num / 2) || (i == num && j <= num / 2) || (j == 1 && i <= num / 2) || (j == num && i > num / 2))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "Enter the number: ";
+    cin >> num;
+
+    print_pattern(num);
+
+    return 0;
+}
