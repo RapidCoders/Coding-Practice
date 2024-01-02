5fc730ef71a345c0fc128065172d91e68cad5015
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-02 22:23:49 +0530

    Create Program1.cpp

---
+++ DAY_32/Shreyash/Program1.cpp
@@ -0,0 +1,13 @@
+// Q.1 Write a function that takes an array of integers and returns the length of the longest consecutive elements sequence.
+// Input: [100, 4, 200, 1, 3, 2]
+// Output: 4 (the longest consecutive elements sequence is [1, 2, 3, 4])
+
+// Input: [9, 1, 4, 7, 2, 8, 3, 5]
+// Output: 5 (the longest consecutive elements sequence is [1, 2, 3, 4, 5])
+
+// Input: [3, 3, 2, 2, 1, 1]
+// Output: 3 (longest consecutive sequence is [1, 2, 3])
+
+// Input: [10, 20, 30, 40, 50]
+// Output: 5 (longest consecutive sequence is [10, 20, 30, 40, 50])
+// Author: Vaibhav
