c12c69ba1489252588f1d0324d35f3c04d64bfbc
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-03 00:39:44 +0530

    Update Program1.cpp

--- DAY_32/Shreyash/Program1.cpp
+++ DAY_32/Shreyash/Program1.cpp
@@ -11,3 +11,56 @@
 // Input: [10, 20, 30, 40, 50]
 // Output: 5 (longest consecutive sequence is [10, 20, 30, 40, 50])
 // Author: Vaibhav
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int longest_consecutive(vector<int>arr,int num)
+{
+    for(int i = 0;i<arr.size();i++){
+        for(int j = 0;j<arr.size();j++){
+            if(arr[i]  < arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+     int count = 1;
+    int max_count = -1;
+ 
+    for (int i = 0; i < num - 1; i++)
+    {
+        if (arr[i + 1] - arr[i] == 1)
+        {
+            count++;
+        }
+        else if (arr[i + 1] - arr[i] == 0)
+        {
+            continue;
+        }
+        else
+        {
+            count = 1;
+        }
+        max_count = max(max_count, count);
+    }
+    return max_count;
+}
+
+int main()
+{
+    vector<int>arr;
+    int num;
+    cin>>num;
+    int temp;
+    for(int i = 0;i<num;i++){
+        cin>>temp;
+        arr.push_back(temp);
+    }
+
+    int numm = longest_consecutive(arr,num);
+    cout<<numm;
+    return 0;
+}
