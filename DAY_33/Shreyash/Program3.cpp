17acaf338d17b98dc6539702f60a164822294613
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-03 10:23:25 +0530

    Create Program3.cpp

---
+++ DAY_33/Shreyash/Program3.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void even_odd(int mat[3][3],int row)
+{
+    vector<int>even;
+    vector<int>odd;
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<row;j++){
+            if(mat[i][j]%2 == 0){
+                even.push_back(mat[i][j]);
+
+            }
+            else{
+                odd.push_back(mat[i][j]);
+            }
+        }
+    }
+    cout<<"Even Numbers in matrix :";
+    for(int k:even){
+        cout<<k<<" ";
+    }
+    cout<<endl;
+    cout<<"odd Numbers in matrix :";
+    for(int l:odd){
+        cout<<l<<" ";
+    }
+    
+    
+}
+
+int main()
+{
+    int mat[3][3];
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cin>>mat[i][j];
+        }
+    }
+
+    even_odd(mat,row);
+    return 0;
+}
