2c3ca86b06962426c72b545da098199b5d7b3696
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-03 23:00:42 +0530

    Update Program1.cpp

--- DAY_33/Shreyash/Program1.cpp
+++ DAY_33/Shreyash/Program1.cpp
@@ -1,1 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int majority(vector<int>arr,int num,int siz)
+{
+    int count = 0;
+    int maj;
+    for(int i = 0;i<num;i++)
+    {
+        for(int j = 0;j<num;j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if(count >= siz)
+        {
+            return arr[i];
+            break;
+        }
+        count = 0;
+        
+    }
+    return 0;
+
+}
+
+int main()
+{
+    vector<int>arr;
+    int num;
+    cin>>num;
+    int temp;
+    for(int i = 0;i<num;i++)
+    {
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int siz = num/2;
+    int result = majority(arr,num,siz);
+    if(result == 0)
+    {
+        cout<<"None";
+    }else{
+        cout<<result;
+    }
+    
+    return 0;
+}
