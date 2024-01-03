226b1e26bf4c00548c4f577421ee4c25a540e837
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-03 23:01:41 +0530

    Update Program2.cpp

--- DAY_33/Shreyash/Program2.cpp
+++ DAY_33/Shreyash/Program2.cpp
@@ -1,3 +1,15 @@
+// Q.2 Write a method to determine and return whether a given matrix is a sparse matrix.
+// Explanation: A matrix is said to be sparse matrix if most of the elements of that matrix are 0. It implies that it contains very less non-zero elements. To check whether the given matrix is the sparse matrix or not, we first count the number of zero elements present in the matrix. Then calculate the size of the matrix. For the matrix to be sparse, count of zero elements present in an array must be greater than size/2.
+
+// Given Matrix : {4  0  0}
+//                {0  5  0}
+//                {0  0  6}
+
+// Number of zeroes present in above matrix is 6 and size of the matrix is 3 * 3 = 9.
+// Since, 6 > 4.5 that means, most elements of given array are zero.
+// Hence, the above matrix is a sparse matrix.
+// Author: Suyash
+
 #include<iostream>
 #include<vector>
 using namespace std;
