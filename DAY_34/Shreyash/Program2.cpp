b6786862969c1f187533e94c0fda4d4e9e04f0a7
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-04 11:39:18 +0530

    Update Program2.cpp

--- DAY_34/Shreyash/Program2.cpp
+++ DAY_34/Shreyash/Program2.cpp
@@ -1,3 +1,18 @@
+
+// Q.2 Program to Print Matrix in Z Form.
+// Explanation: The Z form includes the elements of the first row, right diagonal, and the last row of the matrix. To print the Z form of the matrix, the rows and columns must be the same.
+
+// Input : {5 9 1}
+//         {3 6 7}
+//         {4 8 0}
+
+// Output : 5 9 1
+//            6
+//          4 8 0 
+         
+// Author: Suyash
+
+
 #include<iostream>
 using namespace std;
 
