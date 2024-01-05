d76124ae129789d942d0158532bd08f26e7e87c0
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-05 15:25:31 +0530

    Update Program2.cpp

--- DAY_35/Shreyash/Program2.cpp
+++ DAY_35/Shreyash/Program2.cpp
@@ -1,1 +1,18 @@
 
+// Q.2 Program to chech whether Matrix is Trace Matrix.
+// Explanation: The normal of a matrix is the square root of the sum of squares of all the elements of a matrix. For example, consider the following matrix. {9 8 2} {1 4 7} {3 5 6} Normal and Trace of a Matrix in Java First, we will calculate the sum of the square of each element. 92 + 82 + 22 + 12 + 42 + 72 + 32 + 52 + 62 81 + 64 + 4 + 1 + 16 + 49 + 9 + 25 + 36 = 285 Now, calculate the square root of the sum of squares. √285 = 16.8819430161
+
+// The trace of a matrix is the sum of all the elements present in the principal diagonal (upper left to lower right). Note that the matrix must be a square matrix (the number of rows and columns must be the same). It is useful to prove results in linear algebra. For example, consider the following matrix. {(9) 8 2} {1 (4) 7} {3 5 (6)} Normal and Trace of a Matrix in Java Trace for the above matrix is 5 + 4 + 7 = 16.
+
+// Author: Suyash
+
+// Q.3 Program to Convert Decimal to Hexadecimal.
+// Input:  num = 10
+// Output: a.
+
+// Input:  num = 15
+// Output: f.
+
+// Input:  num = 289
+// Output: 121.
+// Author: Shreyash
