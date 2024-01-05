1074c7a541983ab6a0c376768b6074e0cd51863b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-05 15:25:51 +0530

    Update Program3.cpp

--- DAY_35/Shreyash/Program3.cpp
+++ DAY_35/Shreyash/Program3.cpp
@@ -1,1 +1,11 @@
 
+// Q.3 Program to Convert Decimal to Hexadecimal.
+// Input:  num = 10
+// Output: a.
+
+// Input:  num = 15
+// Output: f.
+
+// Input:  num = 289
+// Output: 121.
+// Author: Shreyash
