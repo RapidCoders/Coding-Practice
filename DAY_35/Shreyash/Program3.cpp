fab5a8e9ec3fb164e8c7374e46cd71d8638855f1
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-05 22:21:09 +0530

    Update Program3.cpp

--- DAY_35/Shreyash/Program3.cpp
+++ DAY_35/Shreyash/Program3.cpp
@@ -9,3 +9,62 @@
 // Input:  num = 289
 // Output: 121.
 // Author: Shreyash
+
+#include<iostream>
+using namespace std;
+
+string decimaltohexa(int num)
+{
+    string str = "";
+    int reminder;
+    int result;
+    while(num > 0)
+    {
+        reminder = num%16;
+        if(reminder == 10){
+            str = str + "a";
+        }
+        else if(reminder == 11)
+        {
+            str = str + "b";
+
+        }
+        else if(reminder == 12)
+        {
+            str = str + "c";
+
+        }
+        else if(reminder == 13)
+        {
+            str = str + "d";
+
+        }
+        else if(reminder == 14)
+        {
+            str = str + "e";
+
+        }
+        else if(reminder == 15)
+        {
+            str = str + "f";
+
+        }
+        else{
+            str = str + to_string(reminder);
+        }
+        num = num/16;
+    }
+
+
+    return str;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter Decimal Number :";
+    cin>>num;
+    string result = decimaltohexa(num);
+    cout<<result; 
+    return 0;
+}
