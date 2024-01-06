f68f8da41521ca2319721c0a2e156de4228532cc
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-06 21:44:50 +0530

    Update Program2.cpp

--- DAY_36/Shreyash/Program2.cpp
+++ DAY_36/Shreyash/Program2.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
 
+bool ducknum(int num)
+{
+    int mod;
+    while(num>0)
+    {
+        mod = num%10;
+        if(mod == 0)
+        {
+            return true;
+        }
+        num = num/10;
+
+    }
+    return false;
+
+}
+
+int main()
+{
+    int num;
+    cout<<"num :";
+    cin>>num;
+    if(ducknum(num))
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+
+    return 0;
+}
