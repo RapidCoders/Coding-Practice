1c1bfcd94d331256c7f430221cb482d6d28045f4
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-06 22:32:31 +0530

    Update Program1.cpp

--- DAY_36/Shreyash/Program1.cpp
+++ DAY_36/Shreyash/Program1.cpp
@@ -1,3 +1,40 @@
+// Q.1 Write a program to determine if a given matrix is symmetric. A matrix is symmetric if it is equal to its transpose.
+// Input:  [1, 2, 3]
+//         [2, 4, 5]
+//         [3, 5, 6]
+// Output: True.
+
+// Input:  [1, 2, 3]
+//         [2, 4, 5]
+//         [3, 5, 6]
+//         [4, 8, 9]
+// Output: False.
+
+// Input:  [1, 2, 3]
+//         [2, 1, 0]
+//         [3, 0, 1]
+// Output: True.
+
+// Input:  [1, 2, 3]
+//         [4, 5, 6]
+//         [7, 8, 9]
+// Output: False.
+// Author: Vaibhav
+
+
+Q.3 Program to Calculate HCF of Two Number.
+Input:   num1 = 36
+         num2 = 60
+
+Output: 12
+
+Input:   num1 = 56
+         num2 = 12
+
+Output: 4
+
+Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
