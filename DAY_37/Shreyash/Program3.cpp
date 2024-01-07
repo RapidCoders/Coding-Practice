561ed2a69e1c02de34eb02480c56deadf5803b9b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-07 15:34:55 +0530

    Update Program3.cpp

--- DAY_37/Shreyash/Program3.cpp
+++ DAY_37/Shreyash/Program3.cpp
@@ -11,3 +11,37 @@
 // Output: 165
 
 // Author: Shreyash
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int lcm(int num1,int num2)
+{
+    int maxx = max(num1,num2);
+    int lcm = 1;
+    for(int i = maxx;i<num1*num2;i++){
+        if(i%num1 == 0&&i%num2 == 0)
+        {
+            lcm = i;
+            
+        }
+
+    }
+
+    return lcm;
+    
+}
+
+int main()
+{
+    int num1;
+    cout<<"num1 :";
+    cin>>num1;
+    int num2;
+    cout<<"num2 :";
+    cin>>num2;
+    int result = lcm(num1,num2);
+    cout<<"HCF for given Inputs: "<<result; 
+    return 0;
+}
