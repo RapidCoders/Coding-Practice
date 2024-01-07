e52d9f987a0ff97c42b025993f7881c7a63bceac
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-07 15:31:26 +0530

    Update Program1.cpp

--- DAY_37/Shreyash/Program1.cpp
+++ DAY_37/Shreyash/Program1.cpp
@@ -1,1 +1,17 @@
+// Q.1 Write a program to count the number of set bits (1s) in the binary representation of a given non-negative integer.
+// Input:  0
+// Output: 0
+
+// Input:  7
+// Output: 3
+
+// Input:  123
+// Output: 6
+
+// Input:  1024
+// Output: 1
+
+// Input:  65535
+// Output: 16
+// Author: Vaibhav
 
