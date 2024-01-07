61f52bba806d2a528fd3742ed87976cc6ced1c1d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-07 15:32:13 +0530

    Create Program3.cpp

---
+++ DAY_37/Shreyash/Program3.cpp
@@ -0,0 +1,13 @@
+
+// Q.3 write Program to find the LCM of two numbers
+// Input:   num1 = 12
+//          num2 = 14
+
+// Output: 84
+
+// Input:   num1 = 55
+//          num2 = 15
+
+// Output: 165
+
+// Author: Shreyash
