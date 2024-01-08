f63b6b6d9758c91921877432bcfb113a52fc6ef5
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-09 00:17:59 +0530

    Update Program3.cpp

--- DAY_38/Vaibhav/Program3.cpp
+++ DAY_38/Vaibhav/Program3.cpp
@@ -1,1 +1,43 @@
+/*
+Q.3 write Program for Octal to Decimal Conversion.
 
+Input: octal = 462
+Output:  306
+
+Input: octal = 206
+Output: 134
+
+Author: Shreyash
+*/
+
+#include <iostream>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int oct_to_dec(int num)
+{
+    int x = 0, ans = 0;
+
+    while (num)
+    {
+        ans += ((num % 10) * pow(8, x));
+        num /= 10;
+        x++;
+    }
+    return ans;
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "\nEnter the number: ";
+    cin >> num;
+
+    cout << "\nDecimal form of " << num << " is " << oct_to_dec(num) << endl;
+
+    return 0;
+}
