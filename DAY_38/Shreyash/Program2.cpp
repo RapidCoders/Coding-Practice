16e4a3769b1b4a2dc0ec0f38d754bbe7def7548a
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-08 23:47:52 +0530

    Update Program2.cpp

--- DAY_38/Shreyash/Program2.cpp
+++ DAY_38/Shreyash/Program2.cpp
@@ -1,1 +1,16 @@
 
+// Q.2 Write a method to check and return whether given number is Buzz Number or not.
+// Explanation:
+// Buzz number is another special number in Java that ends with digit 7 or divisible by 7. Unlike Prime and Armstrong numbers, the Buzz number is not so popular and asked by the interviewers.
+
+// In simple words, a number is said to be Buzz if it ends with 7 or is divisible by 7.
+
+// Let's take some examples of Buzz numbers.
+
+// 42 is a Buzz number because it is divisible by 7.
+// 107 is a Buzz number because it ends with 7.
+// 147 is a Buzz number because it ends with 7 and also divisible by 7.
+// 134 is not a Buzz number because it is neither end with 7 nor divisible by 7.
+
+// Author: Suyash
+
