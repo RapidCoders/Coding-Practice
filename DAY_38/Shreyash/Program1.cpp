e4fe091587e8c87c05bd4ac1f884a25f8e782750
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-08 23:47:35 +0530

    Update Program1.cpp

--- DAY_38/Shreyash/Program1.cpp
+++ DAY_38/Shreyash/Program1.cpp
@@ -1,1 +1,11 @@
+// Q.1 Write a program to calculate, a) area of rectangle b) perimeter of rectangle c) check it is square or not
+// Input:  length = 4.0, breadth = 6.0
+// Output: Area: 24.0, Perimeter: 20.0, is_square: No.
+
+// Input:  length = 7.5, breadth = 7.5
+// Output: Area: 56.25, Perimeter: 30, is_square: Yes.
+
+// Input:  length = 3.0, breadth = 9.0
+// Output: Area: 27.0, Perimeter: 24.0, is_square: No.
+// Author: Vaibhav
 
