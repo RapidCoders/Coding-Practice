0921f22ec48ad256a78b2f484d9b3b58ed16ddeb
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-08 23:50:13 +0530

    Update Program1.cpp

--- DAY_38/Shreyash/Program1.cpp
+++ DAY_38/Shreyash/Program1.cpp
@@ -9,3 +9,38 @@
 // Output: Area: 27.0, Perimeter: 24.0, is_square: No.
 // Author: Vaibhav
 
+#include<iostream>
+using namespace std;
+
+float arearectangle(float len,float breadth){
+    float area;
+    area = len*breadth;
+    return area;
+}
+
+float perimeter(float len,float breadth){
+    float perimeter;
+    perimeter = 2*(len+breadth);
+    return perimeter;
+}
+
+int main()
+{
+    float len;
+    cout<<"Length :";
+    cin>>len;
+    float breadth;
+    cout<<"Length :";
+    cin>> breadth;
+    cout<<"Area of Reactangle :"<<arearectangle(len,breadth)<<endl;
+    cout<<"Perimeter of Rectangle :"<<perimeter(len,breadth);
+    if(len == breadth)
+    {
+      cout<<"is_square :"<<"True";
+    }
+    else
+    {
+      cout<<"is_square :"<<"False";
+    }
+    return 0;
+}
