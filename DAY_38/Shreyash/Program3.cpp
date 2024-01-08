d826c3c20c67b707c127b78fe1e83479595384c4
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-08 23:51:19 +0530

    Update Program3.cpp

--- DAY_38/Shreyash/Program3.cpp
+++ DAY_38/Shreyash/Program3.cpp
@@ -7,3 +7,35 @@
 // Output: 134
 
 // Author: Shreyash
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int octaltodecimal(int num)
+{
+    int mod;
+    int sum = 0;
+    int power = 0;
+    while(num > 0)
+    {
+        mod = num%10;
+        int sqr;
+        sqr = pow(8,power);
+        sum = sum + (sqr * mod);
+        num = num/10;
+        power++;
+    }
+    return sum;
+    
+}
+
+int main()
+{
+    int num;
+    cout<<"Octal :";
+    cin>>num;
+    int result = octaltodecimal(num);
+    cout<<"Decimal : "<<result; 
+    return 0;
+}
