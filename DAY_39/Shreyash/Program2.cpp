35ac837b93025a6802e3f9a920f1ad27238c4027
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-09 23:15:32 +0530

    Update Program2.cpp

--- DAY_39/Shreyash/Program2.cpp
+++ DAY_39/Shreyash/Program2.cpp
@@ -1,3 +1,22 @@
+// Q.2 Write a method to check and return whether given number is Spy Number or not.
+// Explanation:
+// A positive integer is called a spy number if the sum and product of its digits are equal.
+// In other words, a number whose sum and product of all digits are equal is called a spy number.
+
+// Example:
+// Let's take the number 1124 and check whether the number is a spy or not.
+// First, we will split it into digits (1, 1, 2, 4). After that find the sum and product of all the digits.
+//   Sum = 1 + 1 + 2 + 4 = 8
+//   Product = 1 * 1 * 2 * 4 = 8
+// We observe that the sum and product of the digits both are equal. Hence, 1124 is a spy number.
+
+// Input  : 132
+// Output : Spy number.
+
+// Input  : 217
+// Output : Not a Spy Number.
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
