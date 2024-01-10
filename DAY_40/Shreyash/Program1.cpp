f9db2f7906a37dbcf67d6c099d697e8024511c68
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-10 23:52:08 +0530

    Update Program1.cpp

--- DAY_40/Shreyash/Program1.cpp
+++ DAY_40/Shreyash/Program1.cpp
@@ -1,65 +1,98 @@
-// Q.1 Write a program to implement a function that checks if a given string is a pangram. A pangram is a sentence or expression that uses every letter of the alphabet at least once.
-// Input: "The quick brown fox jumps over the lazy dog."
-// Output: True
+/*
+Q.1 Write a program to implement a function that finds the second-largest element in an array. The function should take an array of integers as input and return the second-largest element. If there is no second-largest element, return -1.
 
-// Input:  "Pack my box with five 7623 dozen liquor jugs."
-// Output: True
+Input: [3, 8, 5, 2, 7, 1, 6, 4]
+Output: 7
 
-// Input:  "The lazy dog slept."
-// Output: False
+Input:  [10, 10, 10, 10, 10]
+Output: -1
 
-// Input:  "Cwm fjord bank glyphs vext quiz."
-// Output: True
-// Author: Vaibhav
+Input:  [-5, -2, 0, 3, 7, 1]
+Output: 3
 
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<set>
-using namespace std;
+Input:  [5, 5, 5, 5, 5, 5, 5, 5]
+Output: -1
 
-int sec_largest(vector<int>arr)
+Author: Vaibhav
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iterator>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+// This solution is also working fine.
+// int find_second_largest(const vector<int> &nums)
+// {
+//     std::set uniques(nums.begin(), nums.end());
+
+//     if (uniques.size() > 1)
+//         return *std::prev(uniques.end(), 2);
+//     return -1;
+// }
+
+int find_largest(const vector<int> &nums)
 {
-    set<int>arr_uni;
-    
-    for(int k : arr)
+    int largest = nums[0];
+
+    for (int x : nums)
     {
-        for(int j: arr)
-        {
-            if(k > j)
-            {
-                int temp = k;
-                k = j;
-                j = temp;
-            }
-        }
+        if (largest < x)
+            largest = x;
     }
-    for(int k : arr){
-        arr_uni.insert(k);
-    }
-    if(arr_uni.size() < arr.size()){
-        return -1;
-    }else{
-        return arr[arr_uni.size() - 2];
-    }
-    
+    return largest;
 }
 
-int main()
+int find_second_largest(const vector<int> &nums)
 {
-    vector<int>arr;
-    int num;
-    cout<<"Enter How Many Numbers U want to put in array :";
-    cin>>num;
-    int temp;
-    for(int i = 0;i<num;i++)
+    int largest, second_largest;
+    largest = second_largest = find_largest(nums);
+
+    for (int x : nums)
     {
-        cin>>temp;
-        arr.push_back(temp);
+        if (x != largest)
+        {
+            second_largest = x;
+            break;
+        }
     }
 
-    int result = sec_largest(arr);
+    if (largest == second_largest)
+        return -1;
 
-    cout<<"Second Largest Element :"<<result;
+    for (int x : nums)
+    {
+        if (second_largest < x && x != largest)
+            second_largest = x;
+    }
+    return second_largest;
+}
+
+int main(void)
+{
+    int i, x, n, ret_val;
+    vector<int> nums;
+
+    cout << "\nEnter the number of elements: ";
+    cin >> n;
+
+    cout << "\nEnter the data: ";
+    for (i = 1; i <= n; i++)
+    {
+        cin >> x;
+        nums.push_back(x);
+    }
+
+    ret_val = find_second_largest(nums);
+
+    if (ret_val != -1)
+        cout << "\nThe second largest element from array is: " << ret_val << endl;
+    else
+        cout << "\nThere is no second largest element in array.\n";
+
     return 0;
 }
