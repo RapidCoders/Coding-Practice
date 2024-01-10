37524921f7d488ae04e73a5bd495bb55713f0d97
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-10 09:34:50 +0530

    Update Program1.cpp

--- DAY_40/Shreyash/Program1.cpp
+++ DAY_40/Shreyash/Program1.cpp
@@ -1,1 +1,14 @@
+// Q.1 Write a program to implement a function that checks if a given string is a pangram. A pangram is a sentence or expression that uses every letter of the alphabet at least once.
+// Input: "The quick brown fox jumps over the lazy dog."
+// Output: True
+
+// Input:  "Pack my box with five 7623 dozen liquor jugs."
+// Output: True
+
+// Input:  "The lazy dog slept."
+// Output: False
+
+// Input:  "Cwm fjord bank glyphs vext quiz."
+// Output: True
+// Author: Vaibhav
 
