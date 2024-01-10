e64ff7972b15fde27c2bc6cc5c4824f0fb85035c
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-10 09:35:18 +0530

    Update Program2.cpp

--- DAY_40/Shreyash/Program2.cpp
+++ DAY_40/Shreyash/Program2.cpp
@@ -1,3 +1,14 @@
+
+// Q.2 Program to Convert meter to Feet and Inches.
+// Input  : 40
+// Output : The value of 40 meter in Inches is:  1574.80
+//          The value of 40 meter in feet is: 131.24
+
+// Input  : 10
+// Output : The value of 10 meter in Inches is: 393.7
+//          The value of 10 meter in Feet is: 32.81
+// Author: Suyash
+
 #include<iostream>
 using namespace std;
 
