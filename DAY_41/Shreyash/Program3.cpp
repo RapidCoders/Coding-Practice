84f1e019ebc38bc753b21f9271afd6f0a6e82051
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-11 16:03:02 +0530

    Update Program3.cpp

--- DAY_41/Shreyash/Program3.cpp
+++ DAY_41/Shreyash/Program3.cpp
@@ -1,3 +1,23 @@
+
+// Q.3 write program to Print Number Star Right Triangle Pattern.
+// Input: n = 4
+// Output:
+
+//          1
+//          3*2
+//          6*5*4
+//          10*9*8*7
+
+// Input: n = 5
+// Output:
+//          1
+//          3*2
+//          6*5*4
+//          10*9*8*7
+//          15*14*13*12*11
+// Author: Shreyash
+
+
 #include<iostream>
 #include<cmath>
 using namespace std;
