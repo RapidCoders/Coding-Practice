9088f1a931b5d1e1eaf37ff0dc944515547d7034
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-12 23:33:44 +0530

    Update Program1.cpp

--- DAY_42/Shreyash/Program1.cpp
+++ DAY_42/Shreyash/Program1.cpp
@@ -1,3 +1,21 @@
+// Q.1 Write a program to implement binary search on a sorted array. The program should take an array of integers and a target value as input, and return the index of the target value in the array. If the target is not present, return -1.
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Target: 6
+// Output: 5
+
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Target: 1
+// Output: 0
+
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Target: 9
+// Output: 8
+
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Target: 10
+// Output: -1
+// Author: Vaibhav
+
 #include<iostream>
 #include<vector>
 using namespace std;
