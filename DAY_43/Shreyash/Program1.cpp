1bf20bce3ecf2cb8b0e34ed1c237f95b386e296d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-13 23:17:20 +0530

    Update Program1.cpp

--- DAY_43/Shreyash/Program1.cpp
+++ DAY_43/Shreyash/Program1.cpp
@@ -1,3 +1,18 @@
+// Q.1 Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz." For numbers that are multiples of both three and five, print "FizzBuzz."
+// Input: 3
+// Output: Fizz
+
+// Input: 5
+// Output: Buzz
+
+// Input: 15
+// Output: FizzBuzz
+
+// Input: 7
+// Output: 7
+// Author: Vaibhav
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
