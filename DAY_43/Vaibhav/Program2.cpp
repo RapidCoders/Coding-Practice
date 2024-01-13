9b65ec66d03d3aab9d7c723eeb70806742185735
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-14 01:52:28 +0530

    Update Program2.cpp

--- DAY_43/Vaibhav/Program2.cpp
+++ DAY_43/Vaibhav/Program2.cpp
@@ -1,1 +1,32 @@
+/*
+Q.2 Write a program to find the quotient and remainder of given number.
 
+Input: Enter the numbers: 34  3
+
+Output:
+Quotient when 34/3 is: 11
+Remainder when 34 is divided by 3 is: 1
+
+Author: Suyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+    int num1, num2;
+
+    cout << "\nEnter the divident: ";
+    cin >> num1;
+
+    cout << "\nEnter the divisor: ";
+    cin >> num2;
+
+    cout << "\nQuotient when " << num1 << "/" << num2 << " is: " << num1 / num2 << endl;
+    cout << "\nRemainder when " << num1 << " is divided by " << num2 << " is: " << num1 % num2 << endl;
+
+    return 0;
+}
