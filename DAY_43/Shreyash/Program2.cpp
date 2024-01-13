114201cb9586fbaa3a356ad45a33aefcd816245c
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-13 23:16:29 +0530

    Update Program2.cpp

--- DAY_43/Shreyash/Program2.cpp
+++ DAY_43/Shreyash/Program2.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+void QuoandRemin(int num1,int num2){
+    int Quo = num1/num2;
+    int remin = num1%num2;
+    cout<<"Quotient :"<<Quo<<endl;
+    cout<<"Remainder :"<<remin;
+}
+
+int main(){
+    int num1;
+    cout<<"Num :";
+    cin>>num1;
+    int num2;
+    cout<<"Num2 :";
+    cin>>num2;
+    QuoandRemin(num1,num2);
+    return 0;
+}
