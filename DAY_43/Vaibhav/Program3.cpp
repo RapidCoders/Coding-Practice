e2fc2221650a2938405c8739d83322391faf18c9
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-13 22:14:11 +0530

    Create Program3.cpp

---
+++ DAY_43/Vaibhav/Program3.cpp
@@ -0,0 +1,36 @@
+/*
+Q.3 write Recursive program to Calculate Factorial of given number.
+
+Input: n = 5
+Output: 120
+
+Input: n = 7
+Output: 5040
+
+Author: Shreyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int factorial(int num)
+{
+    if (num == 0)
+        return 1;
+
+    return factorial(num - 1) * num;
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "\nEnter the number: ";
+    cin >> num;
+
+    cout << "\nFactorial of " << num << " is: " << factorial(num) << endl;
+
+    return 0;
+}
