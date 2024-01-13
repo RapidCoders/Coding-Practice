b4efa2e0bb57d9c75dc37e77872803d328527660
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-13 23:18:35 +0530

    Update Program3.cpp

--- DAY_43/Shreyash/Program3.cpp
+++ DAY_43/Shreyash/Program3.cpp
@@ -6,3 +6,25 @@
 // Input: n = 7
 // Output: 5040
 // Author: Shreyash
+
+#include<iostream>
+using namespace std;
+
+int factorial(int num)
+{
+    if(num >= 1){
+        return num * factorial(num-1);
+    }else{
+        return 1;
+    }
+}    
+
+int main()
+{
+    int num;
+    cout<<"num :";
+    cin>>num;
+    int result = factorial(num);
+    cout<<result;
+    return 0;
+}
