b9809f29d21d2a3f93570d212ce3bb5b83314d7c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-14 21:27:41 +0530

    Update Program1.cpp

--- DAY_44/Vaibhav/Program1.cpp
+++ DAY_44/Vaibhav/Program1.cpp
@@ -1,1 +1,47 @@
+/*
+Q.1 Write a program to accepts the number as input from the user and check whether the number is positive or not.
 
+Input: 10
+Output: Positive
+
+Input: -15
+Output: Negative
+
+Input: 0
+Output: Neutral
+
+Author: Vaibhav
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int check_positive(int num)
+{
+    if (num > 0)
+        return 1;
+    else if (num < 0)
+        return -1;
+    return 0;
+}
+
+int main(void)
+{
+    int num, result;
+
+    cout << "\nEnter the number: ";
+    cin >> num;
+
+    result = check_positive(num);
+
+    if (result > 0)
+        cout << "\nPositive\n";
+    else if (result < 0)
+        cout << "\nNegative\n";
+    else
+        cout << "\nNeutral\n";
+
+    return 0;
+}
