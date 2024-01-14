d0c6a080ebe9e7cfb6814a4a90911f4d613b8221
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-14 11:01:52 +0530

    Create Program3.cpp

---
+++ DAY_44/Shreyash/Program3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fibonacciSeries(int num)
+{
+    if((num==1)||(num==0)) {
+      return(num);
+   }else {
+      return(fibonacciSeries(num-1)+fibonacciSeries(num-2));
+   }
+}    
+
+int main()
+{
+    int num;
+    cout<<"num :";
+    cin>>num;
+    for(int i = 0;i < num;i++){
+        cout << " " << fibonacciSeries(i);
+    }
+    // cout<<fibonacciSeries(num-1);
+    return 0;
+}
