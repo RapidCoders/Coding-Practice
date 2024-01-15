b779a445cd04e5cba24b1541eba7f10b4fc8e6a6
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-15 23:54:00 +0530

    Update Program3.cpp

--- DAY_45/Vaibhav/Program3.cpp
+++ DAY_45/Vaibhav/Program3.cpp
@@ -1,1 +1,35 @@
+/*
+Q.3 Write a program to count the digits of a given number using recursion.
 
+Input: n = 15
+Output: 2
+
+Input: n = 3773
+Output: 4
+
+Author: Shreyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int count_digits(int num)
+{
+    if (num == 0)
+        return 0;
+    return 1 + count_digits(num / 10);
+}
+
+int main(void)
+{
+    int num;
+
+    cout << "\nEnter the number: ";
+    cin >> num;
+
+    cout << "\nCount of digits is: " << count_digits(num) << endl;
+
+    return 0;
+}
