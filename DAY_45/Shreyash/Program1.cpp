c30cda074ba06e7fbdc1ee58f4a4b4f988cd4629
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-16 00:12:11 +0530

    Update Program1.cpp

--- DAY_45/Shreyash/Program1.cpp
+++ DAY_45/Shreyash/Program1.cpp
@@ -1,3 +1,14 @@
+// Q.1 Write a program to find the largest prime palindrome less than or equal to a given number n. The program should take the input n and output the largest prime palindrome found.
+// Input: 50
+// Output: 11
+
+// Input: 500
+// Output: 383
+
+// Input: 1000
+// Output: 929
+// Author: Vaibhav
+
 #include<iostream>
 using namespace std;
 
