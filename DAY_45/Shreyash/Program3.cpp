851149382ae84accbfb275ee27b89b8bbf7001d7
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-16 00:12:46 +0530

    Update Program3.cpp

--- DAY_45/Shreyash/Program3.cpp
+++ DAY_45/Shreyash/Program3.cpp
@@ -1,3 +1,12 @@
+
+// Q.3 Write a program to count the digits of a given number using recursion.
+// Input: n = 15
+// Output: 2
+
+// Input: n = 3773
+// Output: 4
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
