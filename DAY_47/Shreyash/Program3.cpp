ee93d1f225e79c5ec2f29363bdb2da0f6d488266
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-17 14:35:42 +0530

    Update Program3.cpp

--- DAY_47/Shreyash/Program3.cpp
+++ DAY_47/Shreyash/Program3.cpp
@@ -1,3 +1,10 @@
+
+// Q.3 Write a program which accept n numbers in array and gives difference between maximum and minimum.
+// Input: n =5
+//        32 332 -23 -154 65
+// Output: 486
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 #include<set>
