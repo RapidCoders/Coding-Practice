4ed75e7334a0f1d2c70cf170ddb5f6b1de4d4870
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-17 22:32:25 +0530

    Update Program2.cpp

--- DAY_47/Shreyash/Program2.cpp
+++ DAY_47/Shreyash/Program2.cpp
@@ -1,1 +1,29 @@
+// Q.2 Write a program to calculate simple interest.
+// Input  : P = 1000
+//          R = 5
+//          T = 5
+// Output : 2500
 
+// Author: Suyash
+
+#include<iostream>
+using namespace std;
+
+int simpleinterest(int p,int r,int t){
+    return p*r*t;
+}
+
+
+int main(){
+    int p;
+    cout<<"Principal balance :";
+    cin>>p;
+    int r;
+    cout<<"Rate :";
+    cin>>r;
+    int t;
+    cout<<"Time(in years) :";
+    cin>>t;
+    cout<<"Simple Interest :"<<simpleinterest(p,r,t)<<endl;
+    return 0;
+}
