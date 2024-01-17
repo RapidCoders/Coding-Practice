6189126da7ea2bca1f72b9a494e082ce4ee938df
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-17 23:52:20 +0530

    Update Program2.cpp

--- DAY_47/Vaibhav/Program2.cpp
+++ DAY_47/Vaibhav/Program2.cpp
@@ -1,1 +1,38 @@
+/*
+Q.2 Write a program to calculate simple interest.
 
+Input  : P = 10000
+         R = 5
+         T = 5
+Output : 2500
+
+Author: Suyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int find_simple_interest(int principal, int rate, int time)
+{
+    return (principal * 5 * 5) / 100;
+}
+
+int main(void)
+{
+    int principal, rate, time;
+
+    cout << "\nEnter the principal: ";
+    cin >> principal;
+
+    cout << "\nEnter the rate: ";
+    cin >> rate;
+
+    cout << "\nEnter the time: ";
+    cin >> time;
+
+    cout << "\nSimple interest is: " << find_simple_interest(principal, rate, time) << endl;
+
+    return 0;
+}
