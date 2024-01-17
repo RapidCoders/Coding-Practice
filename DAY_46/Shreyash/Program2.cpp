47a347567aca477bcfe5724179349e69dd5a2f0d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-17 09:57:31 +0530

    Update Program2.cpp

--- DAY_46/Shreyash/Program2.cpp
+++ DAY_46/Shreyash/Program2.cpp
@@ -1,3 +1,11 @@
+
+// Q.2 Write a program to find the area and perimeter of the semicircle.
+// Input  : 10
+// Output : Area : 157
+//          Perimeter : 51.4
+// Author: Suyash
+
+
 #include<iostream>
 using namespace std;
 
