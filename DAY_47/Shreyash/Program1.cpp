379c0ce6ba6ef4e99356fc26da0abaa7c96bdfbf
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-17 14:35:18 +0530

    Update Program1.cpp

--- DAY_47/Shreyash/Program1.cpp
+++ DAY_47/Shreyash/Program1.cpp
@@ -1,3 +1,17 @@
+// Q.1 Write a function that takes a list of integers as input and returns the sum of all unique numbers in the list. If a number appears more than once, it should only be counted once in the sum.
+// Input: [1, 2, 3, 4, 5]
+// Expected Output: 15  # (1 + 2 + 3 + 4 + 5)
+
+// Input: [2, 2, 2, 2, 2]
+// Expected Output: 2  # (Only one unique number)
+
+// Input: [1, 2, 3, 2, 4, 5, 5, 6]
+// Expected Output: 21  # (1 + 2 + 3 + 4 + 5 + 6)
+
+// Input: [-1, -2, 3, -2, 4, 5, -5, 6]
+// Expected Output: 5  # (-1 + -2 + 3 + 4 + 5 + 6)
+// Author: Vaibhav
+
 #include<iostream>
 #include<vector>
 #include<set>
