7409cbf52a22226a904c278bad141d1097bec308
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-18 19:12:04 +0530

    Update Program3.cpp

--- DAY_48/Shreyash/Program3.cpp
+++ DAY_48/Shreyash/Program3.cpp
@@ -25,3 +25,46 @@ int main()
     patt(num);
     return 0;
 }
+
+
+// OR
+#include<iostream>
+#include<vector>
+using namespace std;
+#include <iostream>
+
+void patt(int n)
+{
+    int i, j;
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n*2; j++) {
+            if (j <= i || j >= (n*2+1) - i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout <<endl;
+    }
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n*2; j++) {
+            if (j <= (n+1) - i || j >= n + i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout <<endl;
+    }
+}    
+
+int main()
+{
+    int num;
+    cout<<"n :";
+    cin>>num;
+    patt(num);
+    return 0;
+}
