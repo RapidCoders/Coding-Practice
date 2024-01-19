d3cd72152da102da9ee6959d8bd61086aec6412d
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-19 22:10:09 +0530

    Update Program2.cpp

--- DAY_49/Vaibhav/Program2.cpp
+++ DAY_49/Vaibhav/Program2.cpp
@@ -1,1 +1,33 @@
+/*
+Q.2 Write a program to convert USD to INR
 
+Input  : 10
+Output : 831.06
+
+Input  : 750
+Output : 62329.31
+
+Author: Suyash
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+double usd_to_inr(double usd)
+{
+    return usd * 83.11;
+}
+
+int main(void)
+{
+    double usd;
+
+    cout << "\nEnter the amount is USD: ";
+    cin >> usd;
+
+    cout << usd << " USD in INR is: " << usd_to_inr(usd) << endl;
+
+    return 0;
+}
