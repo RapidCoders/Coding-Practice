6239de516145e8e3d4b113ef632d289108e4f52c
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-19 20:28:20 +0530

    Update Program2.cpp

--- DAY_49/Shreyash/Program2.cpp
+++ DAY_49/Shreyash/Program2.cpp
@@ -1,1 +1,8 @@
+Q.2 Write a program to find 3rd largest element in an array.
+Input  : [1,2,5,6,3,2]
+Output : 3rd largest element is : 3
+
+Input  : [44,66,99,77,33,22,55]
+Output : 3rd largest element is : 66
+Author: Suyash
 
