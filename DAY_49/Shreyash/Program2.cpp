e461d3946e7a3f31c7e32cb8e7d4adfc4dc951f4
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-19 22:51:52 +0530

    Update Program2.cpp

--- DAY_49/Shreyash/Program2.cpp
+++ DAY_49/Shreyash/Program2.cpp
@@ -1,8 +1,23 @@
-Q.2 Write a program to find 3rd largest element in an array.
-Input  : [1,2,5,6,3,2]
-Output : 3rd largest element is : 3
+// Q.2 Write a program to find 3rd largest element in an array.
+// Input  : [1,2,5,6,3,2]
+// Output : 3rd largest element is : 3
 
-Input  : [44,66,99,77,33,22,55]
-Output : 3rd largest element is : 66
-Author: Suyash
+// Input  : [44,66,99,77,33,22,55]
+// Output : 3rd largest element is : 66
+// Author: Suyash
 
+#include<iostream>
+using namespace std;
+
+double usdtoinr(int usd){
+    return usd*83.11;
+}
+
+
+int main(){
+    int usd;
+    cout<<"USD :";
+    cin>>usd;
+    cout<<"INR :"<<usdtoinr(usd)<<endl;
+    return 0;
+}
