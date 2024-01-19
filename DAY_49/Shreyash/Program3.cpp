2dc9d9dca59a8b86f0390422201598ad61b65b22
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-19 20:28:43 +0530

    Update Program3.cpp

--- DAY_49/Shreyash/Program3.cpp
+++ DAY_49/Shreyash/Program3.cpp
@@ -1,1 +1,10 @@
+Q.3 Write a Program to check whether given input number is Emirp Number or not.
+Explanation:A number is called an emirp number if we get another prime number on reversing the number itself. In other words, an emirp number is a number that is prime forwards or backward. It is also known as twisted prime numbers.
 
+Input: n = 46
+Output:False
+
+Input: n = 1201
+Output:True
+Author: Shreyash
+  
