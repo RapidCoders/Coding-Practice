1e34a69466448f01a7a77cd5a456729bd177fe23
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-19 20:27:53 +0530

    Update Program1.cpp

--- DAY_49/Shreyash/Program1.cpp
+++ DAY_49/Shreyash/Program1.cpp
@@ -1,1 +1,21 @@
+Q.1 Write a function that takes a sentence as input and returns the count of unique words in the sentence. Ignore case and punctuation when counting words.
+Input: "The cat in the hat, the hat on the mat."
+Expected Output: 7
+
+Input: "The quick brown Fox jumps over the Lazy Dog."
+Expected Output: 8
+
+Input: ""
+Expected Output: 0
+
+Input: "Coding is fun! Coding is creative, and coding is rewarding."
+Expected Output: 6
+
+Input: "The cat in the hat is the cat on the mat."
+Expected Output: 7
+
+Input: "There are 10 types of people in the world: those who understand binary and those who don't."
+Expected Output: 15
+Author: Vaibhav
+
 
