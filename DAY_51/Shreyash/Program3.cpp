463054523e996389b25e79bf1e30c3286259dc6e
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-21 13:29:56 +0530

    Update Program3.cpp

--- DAY_51/Shreyash/Program3.cpp
+++ DAY_51/Shreyash/Program3.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void patt(int num){
+    int count = 0;
+    int count1 = 3;
+    for (int i=0;i<num/2;i++) 
+    {
+        for (int j=0;j<=i;j++)
+        {
+            count++;
+            if (j!=0)
+            {
+                cout<<"*"<<count;
+            }
+            else
+            {
+                cout<<count;
+            }
+        }
+        cout<<endl;
+        for (int j=0;j<=i+2;j++)
+        {
+            count1++;
+            if (j!=0)
+            {
+                cout<<"*"<<count1;
+            }
+            else
+            {
+                cout<<count1;
+            }
+        }
+        cout<<endl;
+    }
+}    
+int main(){
+    int num;
+    cout<<"NUm :";
+    cin>>num;
+    patt(num);
+    return 0;
+}
+
 
