44674212f4834fae51fc90c7b4e6ff7b9187ab81
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-21 13:18:14 +0530

    Update Program2.cpp

--- DAY_51/Shreyash/Program2.cpp
+++ DAY_51/Shreyash/Program2.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void subsets(string str){
+    vector<string>arr;
+    for (int i = 0; i < str.length(); i++) {
+        for (int j = i; j < str.length(); j++) {
+            arr.push_back(str.substr(i, j - i + 1));
+        }
+    }
+    for(string i:arr){
+        cout<<i<<" ";
+    }
+}    
+int main(){
+    string str1;
+    cout<<"Enter the String :";
+    cin>>str1;
+    subsets(str1);
+
+    return 0;
+}
+
 
