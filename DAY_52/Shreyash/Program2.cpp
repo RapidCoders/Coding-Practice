1e684dd361c31cc07827ef2cff4f707e9365dff1
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-22 17:32:01 +0530

    Update Program2.cpp

--- DAY_52/Shreyash/Program2.cpp
+++ DAY_52/Shreyash/Program2.cpp
@@ -1,1 +1,9 @@
+// Q.2 Write a program to find all possible subsets of a string.
+// Input  : "FUN"  
+// Output :  F, U, N, FU, UN, FUN
+
+// Input  :"ABC"
+// Output : A, AB ,ABC, B, BC, C
+// Author: Suyash
+
 
