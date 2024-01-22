000cebf9ab3bbc09935080186f1e94e0c335e176
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-22 17:54:54 +0530

    Update Program2.cpp

--- DAY_01/Shreyash/Program2.cpp
+++ DAY_01/Shreyash/Program2.cpp
@@ -1,5 +1,13 @@
+// Q2. Write a program to count number of digits in given number.
+// Input: 4365
+// Output: Number of digits are 4.
+
+// Input: 125
+// Output: Number of digits are 3.
+// Author: Suyash
+
+
 #include<iostream>
-#include<map>
 using namespace std;
 
 int counter(int num)
