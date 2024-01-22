433fa90319a56f10a9be148b12a6c00b4644e264
Suyash Sadashiv Awate <suyashsadashivawate@gmail.com>
2024-01-22 18:48:38 +0530

    Create Program2.java

---
+++ DAY_52/Suyash/Program2.java
@@ -0,0 +1,52 @@
+/*
+	Q.2 Write a program to print following pattern.
+	Input : 5
+	Output :
+	1 1 1 1 1
+	1       1
+	1       1
+	1       1
+	1 1 1 1 1
+	
+	Author: Suyash
+*/
+
+import java.util.Scanner;
+
+public class Program2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter a number");
+		int n = sobj.nextInt();
+		
+		PatternPrinting1 pobj1 = new PatternPrinting1();
+		pobj1.PrintPattern(n);;
+		
+		sobj.close();
+	}
+}
+
+class PatternPrinting1
+{
+	public void PrintPattern(int n)
+	{
+		 for (int i = 1; i <= n; i++)   
+		 {   
+			 for (int j = 1; j <= n; j++)   
+		     {   
+				 if (i==1 || i==n || j==1 || j==n)  
+				 {
+					 System.out.print(" 1");   
+				 }
+				 else  
+				 {
+					 System.out.print("  ");  
+				 }
+		     }   	
+		     System.out.println();   
+		 }  
+	}
+}
