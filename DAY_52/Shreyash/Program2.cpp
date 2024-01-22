2373f8174fcd31692eb8894e0209b43e4fa310de
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-22 18:40:47 +0530

    Update Program2.cpp

--- DAY_52/Shreyash/Program2.cpp
+++ DAY_52/Shreyash/Program2.cpp
@@ -6,4 +6,34 @@
 // Output : A, AB ,ABC, B, BC, C
 // Author: Suyash
 
+#include<iostream>
+using namespace std;
+
+void patt(char alpha,int num){
+    for(int i = 1;i<=num;i++)
+    {
+        for(int k = 1;k<=num;k++)
+        {
+            if(i == 1||k == 1||i == num||k == num)
+            {
+                cout<<alpha<<" ";
+            }else{
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}    
+int main(){
+    int num;
+    cout<<"NUm :";
+    cin>>num;
+    char alpha;
+    cout<<"Alpha :";
+    cin>>alpha;
+    patt(alpha,num);
+    return 0;
+}
+
 
