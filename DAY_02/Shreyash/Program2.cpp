0c7064133075bbcb568242a0cbc70d2df5f9813f
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-22 17:53:19 +0530

    Update and rename Program2.py to Program2.cpp

---
+++ DAY_02/Shreyash/Program2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void patt(int num)
+{
+  for(int i = 1;i<=num;i++){
+    for(int j = 1;j<=i;j++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  
+}
+
+int main()
+{
+  int num;
+  cout<<"Enter Num of Rows :";
+  cin>>num;
+  patt(num);
+  return 0;
+}
