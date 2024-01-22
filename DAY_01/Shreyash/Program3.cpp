6fabf88d581769c4bcddc5fad5bc58d0765611ea
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-22 17:55:07 +0530

    Update Program3.cpp

--- DAY_01/Shreyash/Program3.cpp
+++ DAY_01/Shreyash/Program3.cpp
@@ -1,3 +1,11 @@
+// Q3. Write a program which takes a year as input and check whether it is leap year or not.
+// Input: 2000
+// Output: True.
+
+// Input: 2013
+// Output: False.
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
