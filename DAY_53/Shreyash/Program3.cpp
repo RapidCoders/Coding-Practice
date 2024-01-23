a4037100b5e0c19051058b58ccf8d87d80ad686e
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-24 00:21:46 +0530

    Update Program3.cpp

--- DAY_53/Shreyash/Program3.cpp
+++ DAY_53/Shreyash/Program3.cpp
@@ -1,3 +1,21 @@
+// Q.3 Write a program to check whether given number is Xylem or Phloem Number.
+// Explaination:
+// a) Extream digits means first and last digits.
+// b) Mean digits means other than extream digits.
+
+// Input: Enter a number: 825122
+// Output:
+// The sum of extreme digits: 10
+// The sum of mean digits: 10
+// 825122 is a xylem number.
+
+// Input: Enter a number: 761312
+// Output:
+// The sum of extreme digits: 9
+// The sum of mean digits: 11
+// 761312 is a phloem number.
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
