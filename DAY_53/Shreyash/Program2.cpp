5a3b69055a3d53daaff864887da8dc36e9fafc8d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-24 00:20:34 +0530

    Create Program2.cpp

---
+++ DAY_53/Shreyash/Program2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+void patt(int num){
+    for(int i = 1;i<=num;i++)
+    {
+        for(int j = 1;j<=num;j++)
+        {
+            if(i == 1||j == 1||i == num||j == num){
+                cout<<" 1";
+            }
+            else if(i == 2||j==2||i ==num-1||j==num-1 )
+            {
+                cout<<"  ";
+            }
+            else if(i == num/2||j == num/2)
+            {
+                cout<<"  ";
+
+            }
+            else if(i == num/2 + 2||j == num/2 + 2)
+            {
+                cout<<"  ";
+
+            }
+            else if(i == num/2 + 1||j == num/2 + 1){
+                cout<<" 1";
+                
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+
+    }
+    
+    
+    
+}    
+int main()
+{
+    int num;
+    cout<<"Num :";
+    cin>>num;
+    patt(num);
+    return 0;
+}
