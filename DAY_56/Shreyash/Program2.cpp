dc17a5927969385f9b97848c4c2c882f9dea179c
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-26 16:41:19 +0530

    Update Program2.cpp

--- DAY_56/Shreyash/Program2.cpp
+++ DAY_56/Shreyash/Program2.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void patt(int row)
+{
+  for (int i = row; i > 0; i--) 
+  {
+        for (int k = row; k > i; k--) 
+        {
+            cout << "   ";
+        }
+        for (int j = 0; j < i * 2 - 1; j++) 
+        {
+            if (i == row) {
+                cout << " * ";
+            } else {
+                if (j == 0 || j == i * 2 - 2) 
+                {
+                    cout << " * ";
+                } else {
+                    cout << "   ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+int main() {
+    int row;
+    cout << "Enter the number of rows : ";
+    cin >> row;
+    patt(row);
+
+    return 0;
+}
 
