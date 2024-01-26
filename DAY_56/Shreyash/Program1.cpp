a228ebda2ad446b548616e18a43d774e863b6311
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-26 16:50:43 +0530

    Update Program1.cpp

--- DAY_56/Shreyash/Program1.cpp
+++ DAY_56/Shreyash/Program1.cpp
@@ -1,3 +1,27 @@
+// Q.1 Write a program that takes two strings, s1 and s2, as input and returns True if s2 is a rotation of s1, and False otherwise. A rotation means shifting all characters of one string to the right by any number of positions.
+// Input: s1 = "hello", s2 = "ohell"
+// Expected Output: True
+
+// Input: s1 = "python", s2 = "onpyth"
+// Expected Output: True
+
+// Input: s1 = "apple", s2 = "apple"
+// Expected Output: True
+
+// Input: s1 = "cat", s2 = "tacocat"
+// Expected Output: False
+
+// Input: s1 = "", s2 = ""
+// Expected Output: True
+
+// Input: s1 = "abc", s2 = "bcd"
+// Expected Output: False
+
+// Input: s1 = "programming", s2 = "program"
+// Expected Output: False
+// Author: Vaibhav
+
+
 #include<iostream>
 using namespace std;
 
