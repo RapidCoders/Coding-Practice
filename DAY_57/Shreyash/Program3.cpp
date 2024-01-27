04b6fa73709ef8301481269a4bee303661c5c1af
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-27 11:47:23 +0530

    Update Program3.cpp

--- DAY_57/Shreyash/Program3.cpp
+++ DAY_57/Shreyash/Program3.cpp
@@ -1,3 +1,13 @@
+// Q.3 Write a program to check the number is spy number or not.
+// Input: num = 1124
+// Output:True
+
+// Explanation :
+// A positive integer is called a spy number if the sum and product of its digits are equal. In other words, a number whose sum and product of all digits are equal is called a spy number.
+// 1+1+2+4 = 8
+// 1*1*2*4 = 8
+// Author: Shreyash
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
