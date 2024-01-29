b515ef415e4177a5b08b64ddc31a405f31a6049a
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-29 13:21:19 +0530

    Update Program1.cpp

--- DAY_59/Shreyash/Program1.cpp
+++ DAY_59/Shreyash/Program1.cpp
@@ -1,3 +1,19 @@
+// Q.1 Write a function that takes a string and two characters as input and swaps all occurrences of the first character with the second character and vice versa.
+// Input: "hello world", 'l', 'o'
+// Expected Output: "heool wlrod"
+
+// Input: "python", 'a', 'b'
+// Expected Output: "python"
+
+// Input: "", 'a', 'b'
+// Expected Output: ""
+
+// Input: "banana", 'a', 'a'
+// Expected Output: "banana"
+
+// Input: "hello$world!", '$', '!'
+// Expected Output: "hello!world$"
+// Author: Vaibhav
 #include<iostream>
 using namespace std;
 
