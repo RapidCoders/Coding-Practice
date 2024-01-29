a6772131127a483811a7ed542dc22847dd9b9757
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-30 00:10:15 +0530

    Update Program2.cpp

--- DAY_57/Shreyash/Program2.cpp
+++ DAY_57/Shreyash/Program2.cpp
@@ -2,3 +2,26 @@
 // Input  : [1, 2, 3, 4, 5]
 // Output : Sum of all the elements of an array: 15
 // Author: Suyash
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int>arr;
+    int num;
+    cin >> num;
+    int temp;
+    int sum = 0;
+    for (int i = 1;i <= num;i++)
+    {
+        cin >> temp;
+        arr.push_back(temp);
+        sum = sum + temp;   
+    }
+
+    cout << sum;
+
+
+    return 0;
+}
