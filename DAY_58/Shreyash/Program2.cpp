d923ff85cf5a11bb5a7a3b3755db42e7ddb48b38
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-01-30 00:30:55 +0530

    Update Program2.cpp

--- DAY_58/Shreyash/Program2.cpp
+++ DAY_58/Shreyash/Program2.cpp
@@ -1,1 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void identity_ornot(int mat1[3][3], const int row, const int col)
+{
+    int zero_counter = 0;
+    int num_counter = 0;
+    for(int i = 0;i<row;i++)
+    {
+        for(int j = 0;j<col;j++)
+        {
+            if(mat1[i][j] == 0)
+                {
+                    zero_counter++;
+                }
+                else{
+                    num_counter++;
+                }
+        }
+        
+    }
+    if(zero_counter > num_counter)
+    {
+        cout<<"Given matrix is an sparse matrix";
+    }
+    else
+    {
+        cout<<"Given matrix is an not sparse matrix";
+    }
+}
+
+int main()
+{
+    int const row = 3;
+    int const col = 3;
+    int mat1[row][col];
+    cout<<"Enter Data of Matrix One :";
+    for(int i = 0;i<row;i++)
+    {
+        for(int j = 0;j<col;j++)
+        {
+            cin>>mat1[i][j];
+        }
+    }
+    identity_ornot(mat1, row, col);
+    
+
+    return 0;
+}
+
+
 
