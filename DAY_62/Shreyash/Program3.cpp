c89bc285890b2c19d672978e9973030c5288fe32
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-01 23:14:05 +0530

    Create Program3.cpp

---
+++ DAY_62/Shreyash/Program3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void armstrong(int i)
+{
+    int sum = 0;
+    int new_i = i;
+    while(i > 0)
+    {
+        int mod = i%10;
+        sum = sum + pow(mod,3);
+        i = i / 10;
+    }
+    if(sum == new_i)
+    {
+        cout << sum << " ";
+    }
+
+}
+
+int main()
+{
+    int low , high;
+    cin >> low >> high;
+    for(int i = low;i<=high;i++)
+    {
+
+        armstrong(i);
+    }
+    return 0;
+}
