463cbc6379d538fcf2197ce5a121267083f91a47
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-05 20:38:14 +0530

    Update Program3.cpp

--- DAY_64/Shreyash/Program3.cpp
+++ DAY_64/Shreyash/Program3.cpp
@@ -1,1 +1,33 @@
+// Q.3 write a program which takes input number and gives sum of all odd numbers upto that number(Note : Solve using Recursion).
+// Input: 10
+// Output: 26
+// Author: Shreyash
 
+#include<iostream>
+using namespace std;
+
+int sum_odd(int num)
+{
+	if(num <= 0)
+	{
+		return 0;
+	}
+	else if (num%2 == 0)
+	{
+		return sum_odd(num - 1);
+	}
+	else
+	{
+		return num + sum_odd(num -2);
+	}
+}
+
+int main()
+{
+	int num;
+	cout << "Enter the Nth term :";
+	cin >> num;
+	int result = sum_odd(num);
+	cout<<"Sum :"<<result;
+	return 0;
+}
