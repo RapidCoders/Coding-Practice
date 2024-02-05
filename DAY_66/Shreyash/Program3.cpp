7e0d0f0eeb41da59cd4a4f435248b4cecbb8b172
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-05 21:51:18 +0530

    Update Program3.cpp

--- DAY_66/Shreyash/Program3.cpp
+++ DAY_66/Shreyash/Program3.cpp
@@ -1,1 +1,34 @@
 
+#include<iostream>
+using namespace std;
+
+void patt(int r)
+{
+	for(int i = r;i <= r*2;i++)
+	{
+		for(int j = r;j <= i;j++)
+		{
+			cout<<i<<" ";
+		}
+
+		cout<<endl;
+	}
+	for(int i = r*2;i >= r;i--)
+	{
+		for(int j = r;j <= i;j++)
+		{
+			cout<<i<<" ";
+		}
+
+		cout<<endl;
+	}
+	
+}
+
+int main()
+{
+	int r;
+	cin >> r;
+	patt(r);
+	return 0;
+}
