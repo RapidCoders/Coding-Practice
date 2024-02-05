d6bb01d5680b5ae61ce32b340e6a0a504abb9cd5
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-05 20:44:49 +0530

    Update Program1.cpp

--- DAY_64/Shreyash/Program1.cpp
+++ DAY_64/Shreyash/Program1.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
+void split(string str)
+{
+    string substr = "";
+    for(char i:str)
+    {
+        if(i != ' ')
+        {
+            substr += i;
+        }
+		else
+		{
+			substr[0] = ::toupper(substr[0]);
+			cout<<substr<<" ";
+            substr = "";
+        }
+    }
+	
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string to split :";
+    getline(cin , str);
+    str = str + ' ';
+    split(str);
+
+    return 0;
+}
