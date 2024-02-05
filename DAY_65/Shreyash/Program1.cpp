37712fb57dd0a3e05377e2771b32fd40a709b673
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-05 21:36:46 +0530

    Update Program1.cpp

--- DAY_65/Shreyash/Program1.cpp
+++ DAY_65/Shreyash/Program1.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
+string concater(string str,string str1)
+{
+    if(str[str.length()-1] != ' ')
+	{
+		return str+" "+str1;
+	}
+	else
+	{
+		return str+str1;
+	}
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string :";
+    getline(cin , str);
+	string str1;
+    cout << "Enter string :";
+    getline(cin , str1);
+    cout<<concater(str,str1);
+
+    return 0;
+}
