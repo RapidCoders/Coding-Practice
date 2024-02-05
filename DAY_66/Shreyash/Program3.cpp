99cdb745101715593f53fb9328ec688dde374afd
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-05 21:51:41 +0530

    Update Program3.cpp

--- DAY_66/Shreyash/Program3.cpp
+++ DAY_66/Shreyash/Program3.cpp
@@ -1,3 +1,18 @@
+// Q.3 write a program Print Right Diamond Number Pattern.
+// Input : First term =  3
+
+// Output :
+
+// 3
+// 4 4
+// 5 5 5
+// 6 6 6 6
+// 6 6 6 6
+// 5 5 5
+// 4 4
+// 3
+
+// Author: Shreyash
 
 #include<iostream>
 using namespace std;
