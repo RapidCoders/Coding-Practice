c2d5c87fb5b610efc45f04b844033849a7a6246a
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-06 10:29:53 +0530

    Update Program1.cpp

--- DAY_66/Shreyash/Program1.cpp
+++ DAY_66/Shreyash/Program1.cpp
@@ -1,3 +1,20 @@
+// Q.1 Write a function that takes a string as input and returns the middle character(s) of the string. If the length of the string is odd, return the middle character. If the length is even, return the middle two characters.
+// Input: "abcde"
+// Expected Output: "c"
+
+// Input: "abcdef"
+// Expected Output: "cd"
+
+// Input: "x"
+// Expected Output: "x"
+
+// Input: ""
+// Expected Output: ""
+
+// Input: "hello world"
+// Expected Output: " "
+// Author: Vaibhav
+
 #include<iostream>
 using namespace std;
 
