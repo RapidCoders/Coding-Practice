93977354a30504843cddff2e5bbdb9ba358e01a6
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-07 00:43:48 +0530

    Update Program3.cpp

--- DAY_68/Shreyash/Program3.cpp
+++ DAY_68/Shreyash/Program3.cpp
@@ -1,3 +1,15 @@
+// Q.3 write a Program for Trapping Rain water problem.
+// Here, in this page we  will discuss one of the famous problem of  Trapping Rain Water in C++ . We are given with n non-negative integers representing an elevation map where the width of each bar is 1, we need to compute how much water it is able to trap after raining.
+
+// Example :
+// Input : arr[5] = {3, 0, 2, 0, 4}
+// Output : 7
+// Explanation : We can trap “3 units” of water between 3 and 2, “1 unit” on top of bar 2 and “3 units” between 2 and 4. See below diagram also.
+
+// Input : arr[7] = {1, 0, 3, 0, 4, 0, 2}
+// Output : 6
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 using namespace std;
