29a44e92dd03e3ee0f1da30ef83f02dd443c0478
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-07 15:20:06 +0530

    Update Program1.cpp

--- DAY_68/Shreyash/Program1.cpp
+++ DAY_68/Shreyash/Program1.cpp
@@ -1,3 +1,20 @@
+// Q.1 Write a function that takes a string as input and returns a compressed version of the string where consecutive repeated characters are replaced by the character followed by the count of repetitions.
+// Input: "aaabbc"
+// Expected Output: "a3b2c1"
+
+// Input: ""
+// Expected Output: ""
+
+// Input: "x"
+// Expected Output: "x1"
+
+// Input: "!@##$$"
+// Expected Output: "!1@1#2$2"
+
+// Input: "abcdefg"
+// Expected Output: "a1b1c1d1e1f1g1"
+// Author: Vaibhav
+
 #include<iostream>
 #include<set>
 using namespace std;
