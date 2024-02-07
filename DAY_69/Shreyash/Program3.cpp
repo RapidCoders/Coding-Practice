de8c9212b104d6cef4d9153de3b851fe727033dd
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-07 23:41:37 +0530

    Update Program3.cpp

--- DAY_69/Shreyash/Program3.cpp
+++ DAY_69/Shreyash/Program3.cpp
@@ -1,3 +1,10 @@
+// Q.3 write a Program to Replace each element by its rank given in array.
+// There is an array of n elements, replace each element of the array by its corresponding rank. The minimum value element will have the highest rank..
+
+// Input : arr[] = { 100, 2, 70, 12 , 90};
+// Output : 5 1 3 2 4
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
