09db0e4ba0b86fe086fd8a5a713e21fcc603d566
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-08 20:19:34 +0530

    Update Program1.cpp

--- DAY_69/Shreyash/Program1.cpp
+++ DAY_69/Shreyash/Program1.cpp
@@ -1,3 +1,12 @@
+// Q.1 Write a program which takes input from the user and print the below pattern.
+// Input: 5
+// Output:
+// 1  2  3  4  5
+// 16 17 18 19 6
+// 15 24 25 20 7
+// 14 23 22 21 8
+// 13 12 11 10 9
+// Author: Vaibhav
 #include<iostream>
 using namespace std;
 
