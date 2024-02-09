ee3b0ec6c71e9f022935eb892d0772858937b320
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-09 15:46:30 +0530

    Update Program3.cpp

--- DAY_70/Shreyash/Program3.cpp
+++ DAY_70/Shreyash/Program3.cpp
@@ -1,3 +1,10 @@
+// Q.3 write a Program to get number is even or odd(Note You are not allowed to use directly % operator).
+// There is an array of n elements, replace each element of the array by its corresponding rank. The minimum value element will have the highest rank..
+
+// Input : num = 44
+// Output : Even
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
