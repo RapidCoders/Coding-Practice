e7be146fecf3cfdf2a8bc9cb14df95285f156fb2
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-10 23:19:45 +0530

    Update Program1.cpp

--- DAY_71/Shreyash/Program1.cpp
+++ DAY_71/Shreyash/Program1.cpp
@@ -1,3 +1,21 @@
+// Q.1 Write a function that takes a list of integers as input and returns the product of all non-zero digits in the list.
+// Input: [0, 2, 3, 0, 4, 5]
+// Expected Output: 120
+
+// Input: [0, 0, 0, 0]
+// Expected Output: 1
+
+// Input: [0, 0, 0, 5]
+// Expected Output: 5
+
+// Input: [-2, 0, -3, 0, 4, -5]
+// Expected Output: -120
+
+// Input: [10, 20, 0, 30, 40]
+// Expected Output: 240000
+// Author: Vaibhav
+
+
 #include<iostream>
 #include<vector>
 
