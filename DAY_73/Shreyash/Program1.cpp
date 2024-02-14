ec6edda2f2446d2d1e41cab00d62f3a2a8ece5b9
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 00:03:22 +0530

    Update Program1.cpp

--- DAY_73/Shreyash/Program1.cpp
+++ DAY_73/Shreyash/Program1.cpp
@@ -1,1 +1,15 @@
+// Q.1 Write a function that takes a string as input and returns the character that appears consecutively the most number of times, along with the count of consecutive occurrences.
+// Input: "hello world"
+// Expected Output: 'l', 2
+
+// Input: "aabbbccccddddd"
+// Expected Output: 'd', 5
+
+// Input: ""
+// Expected Output: '', 0
+
+// Input: "aaaaa"
+// Expected Output: 'a', 5
+// Author: Vaibhav
+
 
