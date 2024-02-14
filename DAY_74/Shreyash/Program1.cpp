93b8c19aec9fab4f32760ec16e0183382da54d2f
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 00:41:58 +0530

    Update Program1.cpp

--- DAY_74/Shreyash/Program1.cpp
+++ DAY_74/Shreyash/Program1.cpp
@@ -1,1 +1,17 @@
+// Q.1 Write a program that takes two arrays of integers as input and returns True if one array is a rotation of the other array, and False otherwise. A rotation occurs when elements are shifted circularly to the left or right.
+// Input: [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]
+// Expected Output: True
+
+// Input: [1, 2, 3, 4, 5], [4, 5, 1, 2, 3]
+// Expected Output: True
+
+// Input: [1, 2, 3, 4, 5], [3, 4, 5, 1, 2]
+// Expected Output: True
+
+// Input: [1, 2, 3, 4, 5], [1, 2, 3, 4, 6]
+// Expected Output: False
+
+// Input: [], []
+// Expected Output: True
+// Author: Vaibhav
 
