da09be646bf8c5b15f7d09d579c53b57ec73085d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 01:02:06 +0530

    Update Program1.cpp

--- DAY_73/Shreyash/Program1.cpp
+++ DAY_73/Shreyash/Program1.cpp
@@ -12,4 +12,54 @@
 // Expected Output: 'a', 5
 // Author: Vaibhav
 
+#include <iostream>
+using namespace std;
+
+pair<char, int> most_appears(const string& str) {
+    if (str.empty()) {
+        return make_pair('\0', 0);
+    }
+
+    char cc = str[0];
+    char maxc = cc;
+    int cco = 1;
+    int maxco = 1;
+
+    for (size_t i = 1; i < str.length(); ++i) {
+        if (str[i] == cc) {
+            ++cco;
+        } else {
+            if (cco > maxco) {
+                maxco = cco;
+                maxc = cc;
+            }
+            cc = str[i];
+            cco = 1;
+        }
+    }
+    if (cco > maxco) {
+        maxco = cco;
+        maxc = cc;
+    }
+
+    return make_pair(maxc, maxco);
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    pair<char, int> result = most_appears(str);
+
+    if (result.second > 1) {
+        cout << result.first << "," << result.second << endl;
+    } else {
+        cout << " " ", 0" << endl;
+    }
+
+    return 0;
+}
+
+
 
