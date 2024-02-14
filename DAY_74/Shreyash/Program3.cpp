3be61caa628553ab57764d4d0dc2c926cbd5cb23
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 00:50:08 +0530

    Update Program3.cpp

--- DAY_74/Shreyash/Program3.cpp
+++ DAY_74/Shreyash/Program3.cpp
@@ -12,3 +12,46 @@
 //  1 2 3 4 5 6 7 8 9 
 
 // Author: Shreyash
+
+#include<iostream>
+using namespace std;
+
+void patt(int n)
+{
+      for (int i = 0; i < 2 * n - 1; i++) 
+      { 
+        int comp; 
+        if (i < n) 
+        { 
+            comp = 2 * i + 1; 
+        } 
+        else 
+        { 
+            comp = 2 * (2 * n - i) - 3; 
+        } 
+        for (int j = 0; j < comp; j++) 
+        { 
+            cout << " "; 
+        } 
+        for (int k = 0; k < 2 * n - comp; k++) 
+        { 
+            if (k == 0 || k == 2 * n - comp - 1 || i == 0 || i == 2 * n - 2) 
+            { 
+                cout << " " << k + 1; 
+            } 
+            else 
+            { 
+                cout << "  "; 
+            } 
+        } 
+        cout << endl; 
+    } 
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  patt(n);
+  return 0;
+}
