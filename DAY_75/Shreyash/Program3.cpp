3c07154afbdd8c554937d196f8cda1f297419c22
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 01:22:39 +0530

    Update Program3.cpp

--- DAY_75/Shreyash/Program3.cpp
+++ DAY_75/Shreyash/Program3.cpp
@@ -1,1 +1,15 @@
+// Q.3 Given two numbers M and N. The task is to find the position of the rightmost different bit in the binary representation of numbers. If both M and N are the same then return -1 in this case.
+// Input: 
+// M = 11, N = 9
+// Output: 
+// 2
+// Explanation: 
+// Binary representation of the given numbers are: 1011 and 1001, 2nd bit from right is different.
 
+// Input: 
+// M = 52, N = 4
+// Output: 
+// 5
+// Explanation: 
+// Binary representation of the given numbers are: 110100 and 0100, 5th-bit from right is different.
+// Author: Shreyash
