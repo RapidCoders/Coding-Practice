ae5ca2245228da15b91a730957c2c387ee032356
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 00:42:15 +0530

    Update Program3.cpp

--- DAY_74/Shreyash/Program3.cpp
+++ DAY_74/Shreyash/Program3.cpp
@@ -1,1 +1,14 @@
+// Q.3 Write a Program to print given pattern.
+// Input : 5
+// Output :
+//  1 2 3 4 5 6 7 8 9 
+//    1           7 
+//      1       5 
+//        1   3 
+//          1 
+//        1   3 
+//      1       5 
+//    1           7 
+//  1 2 3 4 5 6 7 8 9 
 
+// Author: Shreyash
