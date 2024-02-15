eacadbebb2e9c388c6e65e9ff4ee8175d59864eb
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 23:40:08 +0530

    Update Program1.cpp

--- DAY_75/Shreyash/Program1.cpp
+++ DAY_75/Shreyash/Program1.cpp
@@ -17,3 +17,64 @@
 // Input: ""
 // Expected Output: -1
 // Author: Vaibhav
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool is_palindron(string str)
+{
+  string temp = "";
+  for (int i = 0; i < str.length(); i++)
+  {
+    temp = str[i] + temp;
+  }
+
+  if(str == temp)
+  {
+    return true;
+  }
+  return false;
+}
+
+void spliter(string str)
+{
+  vector<string>subs;
+  string temp = "";
+  for(int j = 0;j < str.length();j++)
+  {
+    for(int i = 0; i < str.length();i++)
+    {
+      if(i != j)
+      {
+        temp = temp + str[i];
+      }
+    }
+    subs.push_back(temp);
+    temp = "";
+  }
+  for(int k = 0;k<subs.size();k++)
+  {
+    if(is_palindron(subs[k]))
+    {
+      cout << k << " ";
+    }
+  }
+
+}
+
+int main()
+{
+  string str;
+  cin >> str;
+  if(is_palindron(str))
+  {
+    cout << -1;
+  }
+  else
+  {
+    spliter(str);
+  }
+
+  return 0;
+}
