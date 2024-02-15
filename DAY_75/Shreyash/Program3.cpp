a620f70fa9fcf99362f995a470e0b72c67974727
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-15 23:17:33 +0530

    Update Program3.cpp

--- DAY_75/Shreyash/Program3.cpp
+++ DAY_75/Shreyash/Program3.cpp
@@ -13,3 +13,47 @@
 // Explanation: 
 // Binary representation of the given numbers are: 110100 and 0100, 5th-bit from right is different.
 // Author: Shreyash
+
+#include<iostream>
+using namespace std;
+
+string decimal_binary(int num)
+{
+  int mod;
+  int reminder;
+  string str = " ";
+  while(num>0)
+  {
+    mod = num%2;
+    string character = to_string(mod);
+    num = num/2;
+    // cout<<mod;
+    str =character+str;
+  }
+  return str;
+
+}
+
+int main()
+{
+  int n1;
+  cin >> n1;
+  string num1 = decimal_binary(n1);
+
+  int n2;
+  cin >> n2;
+  string num2 = decimal_binary(n2);
+  for(int i = num1.length();i >= 0;i--)
+  {
+    if(num1[i]==num2[i])
+    {
+      continue;
+    }
+    else
+    {
+      cout << i;
+      break;
+    }
+  }
+  return 0;
+}
