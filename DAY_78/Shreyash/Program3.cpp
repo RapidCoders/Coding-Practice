2ac8a1104382c8c69f54f294a5df4885786d6815
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-17 00:32:37 +0530

    Update Program3.cpp

--- DAY_78/Shreyash/Program3.cpp
+++ DAY_78/Shreyash/Program3.cpp
@@ -1,3 +1,21 @@
+// Q.3 Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+// Input:
+// N = 6
+// arr[] = 7 10 4 3 20 15
+// K = 3
+
+// Output : 7
+// Explanation :
+// 3rd smallest element in the given array is 7.
+
+// Input:
+// N = 5
+// arr[] = 7 10 4 20 15
+// K = 4
+// Output : 15
+// Explanation : 4th smallest element in the given array is 15.
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 using namespace std;
