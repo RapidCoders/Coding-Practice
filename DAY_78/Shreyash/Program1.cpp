e33633a557f8008712bb8b66fa6b2006762890b7
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-17 22:52:01 +0530

    Update Program1.cpp

--- DAY_78/Shreyash/Program1.cpp
+++ DAY_78/Shreyash/Program1.cpp
@@ -1,3 +1,20 @@
+// Q.1 Write a function that takes a positive integer n as input and returns the sum of the squares of all positive integers up to n.
+// Input: 5
+// Output: 55  # 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 1 + 4 + 9 + 16 + 25 = 55
+
+// Input: 10
+// Expected Output: 385
+
+// Input: 1
+// Expected Output: 1
+
+// Input: 0
+// Expected Output: 0
+
+// Input: -5
+// Expected Output: None (or any indication that input is invalid)
+// Author: Vaibhav
+
 #include<iostream>
 
 int sqr_sum(int *num)
