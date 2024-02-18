ff9964bf3cc55ff6d7904bf813f8991cb4b495dd
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-18 23:15:26 +0530

    Update Program3.cpp

--- DAY_79/Shreyash/Program3.cpp
+++ DAY_79/Shreyash/Program3.cpp
@@ -1,3 +1,16 @@
+
+// Q.3 Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+
+// Input: num = 0
+// Output: 0
+// Author: Shreyash
+
 #include<iostream>
 using namespace std;
 
