4360bbdef59c80b23643dc15493b0eee54b1abad
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-18 23:03:00 +0530

    Create Program1.cpp

---
+++ DAY_79/Shreyash/Program1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void palindrom(string str,int &c)
+{
+  string rstr;
+  string ostr;
+  string new_str;
+  for (char i : str)
+    {
+        if (i == '!' || i == ',' ||i == '.' || i == '-' || i == '?'|| i == '\'' || i == ';' || i == '\"'||i == ' ')
+        {
+          rstr = ""+rstr;
+          ostr = ostr + "";
+          continue;
+        }
+        else
+        {
+          rstr =i+rstr;
+          ostr = ostr + i;
+        }
+    }
+  // cout<<rstr;
+  if(ostr == rstr)
+  {
+    c++;
+  }
+
+}
+
+
+void splite(string str)
+{
+    int count = 0;
+    vector<string>arr;
+    string substr = "";
+    for(int i = 0;i <= str.length();i++)
+    {
+        if(str[i] != ' ')
+        {
+            substr += str[i];
+        }
+        else
+        {
+            arr.push_back(substr);
+            substr = "";
+        }
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        palindrom(arr[i],count);
+    }
+
+    cout << count;
+    
+}
+
+int main()
+{
+    string str;
+    str = str + "#";
+    getline(cin,str);
+    splite(str);
+    return 0;
+}
