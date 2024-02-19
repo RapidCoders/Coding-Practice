67d876c0efe297fa6eb2907fcfc754b1156fb217
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-19 18:13:26 +0530

    Update Program3.cpp

--- DAY_80/Shreyash/Program3.cpp
+++ DAY_80/Shreyash/Program3.cpp
@@ -1,3 +1,20 @@
+// Q.3 Given an array of strings arr[] of length n representing non-negative integers, arrange them in a manner, such that, after concatanating them in order, it results in the largest possible number. Since the result may be very large, return it as a string.
+// Input: 
+// n = 5
+// arr[] =  {"3", "30", "34", "5", "9"}
+// Output: "9534330"
+// Explanation: 
+// Given numbers are  {"3", "30", "34", "5", "9"}, the arrangement "9534330" gives the largest value.
+
+// Input: 
+// n = 4
+// arr[] =  {"54", "546", "548", "60"}
+// Output: "6054854654"
+// Explanation: 
+// Given numbers are {"54", "546", "548", "60"}, the arrangement "6054854654" gives the largest value.
+// Author: Shreyash
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
