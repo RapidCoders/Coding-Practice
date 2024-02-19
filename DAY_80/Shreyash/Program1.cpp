2dc93e8f2bbc0cd02994e932945b03f33ccea1ca
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-20 00:48:04 +0530

    Update Program1.cpp

--- DAY_80/Shreyash/Program1.cpp
+++ DAY_80/Shreyash/Program1.cpp
@@ -1,3 +1,20 @@
+// Q.1 Write a function that takes a lowercase letter (a-z) as input and returns its position in the alphabet. Assume that the input will always be a lowercase letter.
+// Input: 'c'
+// Expected Output: 3
+
+// Input: 'a'
+// Expected Output: 1
+
+// Input: 'z'
+// Expected Output: 26
+
+// Input: 'm'
+// Expected Output: 13
+
+// Input: '1' (not a lowercase letter)
+// Expected Output: None (or any indication of invalid input handling)
+// Author: Vaibhav
+    
 #include<iostream>
 
 int position(char c)
