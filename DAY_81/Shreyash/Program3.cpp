4101d8f6b285236dcd2d20d9175f6a31a268b74b
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-21 12:48:18 +0530

    Create Program3.cpp

---
+++ DAY_81/Shreyash/Program3.cpp
@@ -0,0 +1,13 @@
+// Q.3 Given a string S. The task is to print all unique permutations of the given string that may contain dulplicates in lexicographically sorted order.
+// Input: ABC
+// Output: ABC ACB BAC BCA CAB CBA
+// Explanation: Given string ABC has permutations in 6 forms as ABC, ACB, BAC, BCA, CAB and CBA .
+
+// Input: ABSG
+// Output:
+// ABGS ABSG AGBS AGSB ASBG ASGB BAGS 
+// BASG BGAS BGSA BSAG BSGA GABS GASB 
+// GBAS GBSA GSAB GSBA SABG SAGB SBAG 
+// SBGA SGAB SGBA
+// Explanation: Given string ABSG has 24 permutations.
+// Author: Shreyash
