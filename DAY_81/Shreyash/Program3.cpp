663f0a57bda8c7f510af1e005ea95a5661aafe67
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-21 13:00:27 +0530

    Update Program3.cpp

--- DAY_81/Shreyash/Program3.cpp
+++ DAY_81/Shreyash/Program3.cpp
@@ -11,3 +11,49 @@
 // SBGA SGAB SGBA
 // Explanation: Given string ABSG has 24 permutations.
 // Author: Shreyash
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+void swap_char(char &a, char &b) 
+{
+    char temp = a;
+    a = b;
+    b = temp;
+}
+
+void generatePermutations(string str, int left, int right, set<string>& combinations) 
+{
+    if (left == right) 
+    {
+        combinations.insert(str);
+        return;
+    }
+
+    for (int i = left; i <= right; i++) 
+    {
+        swap_char(str[left], str[i]);
+        generatePermutations(str, left + 1, right, combinations);
+        swap_char(str[left], str[i]);
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+
+    set<string>combinations;
+
+    generatePermutations(str, 0, str.size() - 1, combinations);
+
+    for (string s: combinations) {
+        cout << s << endl;
+    }
+
+    cout << combinations.size();
+
+    return 0;
+}
