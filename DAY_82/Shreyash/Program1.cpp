0f536c4967fe8a882a2d83a2857acf9054d19f7a
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-25 15:03:16 +0530

    Update Program1.cpp

--- DAY_82/Shreyash/Program1.cpp
+++ DAY_82/Shreyash/Program1.cpp
@@ -14,3 +14,42 @@
 // Input: "leetcode"
 // Expected Output: "leotcede"
 // Author: Vaibhav
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void re_vowels(string &str)
+{
+    vector<int>arr;
+    vector<int>narr;
+
+    for (int i = 0; i <= str.length()/2; i++)
+    {
+        if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u')
+        {
+            arr.push_back(i);
+        }
+    }
+    for (int i = str.length()-1; i >= str.length()/2 ; i--)
+    {
+        if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u')
+        {
+            narr.push_back(i);
+        }
+    }
+    for(int i = 0;i < arr.size();i++)
+    {
+            swap(str[arr[i]], str[narr[i]]);
+    }
+    cout << str;
+    
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    re_vowels(str);
+    return 0;
+}
