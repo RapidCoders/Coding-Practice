00e19471b1373b5e040cd3e2bd228fb80da0c98d
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-25 17:09:01 +0530

    Update Program3.cpp

--- DAY_83/Program3.cpp
+++ DAY_83/Program3.cpp
@@ -1,3 +1,15 @@
+// Q.3 Given a sorted array arr[] of size N. Find the element that appears only once in the array. All other elements appear exactly twice.
+// Input:
+// Input:
+// N = 11
+// arr[] = {1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65}
+// Output: 4
+// Explanation: 4 is the only element that 
+// appears exactly once.
+
+
+// Author: Shreyash
+
 #include<iostream>
 #include<vector>
 using namespace std;
