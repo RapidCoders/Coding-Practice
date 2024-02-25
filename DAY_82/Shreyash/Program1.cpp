aef3f291207d2a0020f63a58ef72e9463b19cc47
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-25 14:19:04 +0530

    Update Program1.cpp

--- DAY_82/Shreyash/Program1.cpp
+++ DAY_82/Shreyash/Program1.cpp
@@ -1,1 +1,16 @@
+// Q.1 Given a string s, your task is to reverse only the vowels in the string.
+// Input: "hello"
+// Expected Output: "holle"
 
+// Input: "xyz"
+// Expected Output: "xyz"
+
+// Input: "a"
+// Expected Output: "a"
+
+// Input: "aeiou"
+// Expected Output: "uoiea"
+
+// Input: "leetcode"
+// Expected Output: "leotcede"
+// Author: Vaibhav
